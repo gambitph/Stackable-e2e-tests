87d985c21060894db77c8fbfae2169eedacf94b1
Nico Mendoza <blankartwp@gmail.com>
2020-12-28 17:50:39 +0800

    added few tests

---
+++ cypress/integration/text.spec.js
@@ -0,0 +1,37 @@
+/**
+ * Internal dependencies
+ */
+import {
+	assertBlockExist, blockErrorTest, switchLayouts, switchDesigns,
+} from '../support/helpers'
+describe( 'Advanced Text', () => {
+	it( 'should show the block', assertBlockExist( 'ugb/text', '.ugb-text' ) )
+
+	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'ugb/text' ) )
+
+	it( 'should switch layout', switchLayouts( 'ugb/text', [
+		'Plain',
+		'Side Title 1',
+		'Side Title 2',
+	] ) )
+
+	it( 'should switch design', switchDesigns( 'ugb/text', [
+		'Angled Advanced Text',
+		'Aspire Advanced Text',
+		'Aurora Advanced Text',
+		'Dare Advanced Text',
+		'Decora Advanced Text',
+		'Detour Advanced Text',
+		'Dim Advanced Text',
+		'Elevate Advanced Text',
+		'Flex Advanced Text',
+		'Glow Advanced Text',
+		'Lounge Advanced Text',
+		'Lume Advanced Text',
+		'Lush Advanced Text',
+		'Peplum Advanced Text',
+		'Proact Advanced Text 1',
+		'Proact Advanced Text 2',
+		'Seren Advanced Text',
+	] ) )
+} )
