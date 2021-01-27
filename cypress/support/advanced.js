2df0c1726f4d11a1fd2b2a54b204fd87b52c5bdf
Nico Mendoza <blankartwp@gmail.com>
2021-01-27 18:30:37 +0800

    added assertion block modules

---
+++ cypress/support/advanced.js
@@ -0,0 +1,250 @@
+/**
+ * External dependencies
+ */
+import {
+	keys, compact,
+} from 'lodash'
+
+/**
+ *
+ * Assertion function for Advanced Tab.
+ *
+ * @param {string} name
+ * @param {Object} options
+ * @param {Function} desktopCallback
+ * @param {Function} tabletCallback
+ * @param {Function} mobileCallback
+ */
+export const assertAdvancedTab = ( name, options = {}, desktopCallback = () => {}, tabletCallback = () => {}, mobileCallback = {} ) => {
+	const {
+		enableMarginTop = true,
+		enableMarginRight = true,
+		enableMarginBottom = true,
+		enableMarginLeft = true,
+		enablePaddingTop = true,
+		enablePaddingRight = true,
+		enablePaddingBottom = true,
+		enablePaddingLeft = true,
+	} = options
+
+	const MAIN_SELECTOR = '.ugb-main-block'
+	const BLOCK_SELECTOR = `.${ name.split( '/' ).join( '-' ) }`
+
+	const callbacks = {
+		Desktop: desktopCallback,
+		Tablet: tabletCallback,
+		Mobile: mobileCallback,
+	}
+
+	const _collapse = ( collapseName = '' ) => {
+		cy.get( '.ugb-inspector-panel-controls', { log: false } )
+			.then( $panel => {
+				if ( $panel.text().includes( collapseName ) ) {
+					cy.collapse( collapseName )
+				}
+			} )
+	}
+
+	const _adjust = ( adjustName, value, options = {}, assertionFunc, args = [] ) => {
+		cy.get( '.ugb-toggle-panel-body.is-opened', { log: false } )
+			.then( $panel => {
+				if ( $panel.text().includes( adjustName ) ) {
+					if ( args.length ) {
+						cy.adjust( adjustName, value, options )[ assertionFunc ]( ...args )
+					} else {
+						cy.adjust( adjustName, value, options )
+					}
+				}
+			} )
+	}
+
+	const _assertAdvancedTab = ( viewport = 'Desktop' ) => {
+		cy.openInspector( name, 'Advanced' )
+		_collapse( 'General' )
+
+		 //Test Block HTML Tag
+		const tags = [ 'div', 'blockquote', 'section', 'article', 'aside', 'main', 'header', 'footer', 'nav', 'address', 'hgroup' ]
+		tags.forEach( tag => {
+			_adjust( 'Block HTML Tag', tag, { viewport }, 'assertHtmlTag', [
+				MAIN_SELECTOR,
+				tag,
+			] )
+		} )
+
+		 //Test Opacity
+		_adjust( 'Opacity', 0.7, { viewport }, 'assertComputedStyle', [ {
+			[ MAIN_SELECTOR ]: {
+				[ `opacity` ]: '0.7',
+			},
+		} ] )
+
+		 //Test Z-index
+		_adjust( 'Z-index', 6, { viewport }, 'assertComputedStyle', [ {
+			[ MAIN_SELECTOR ]: {
+				[ `z-index` ]: '6',
+			},
+		} ] )
+
+		_collapse( 'Block Spacing' )
+
+		// Test Min. Block Height.
+		_adjust( 'Min. Block Height', 850, { viewport }, 'assertComputedStyle', [ {
+			[ BLOCK_SELECTOR ]: {
+				[ `min-height` ]: '850px',
+			},
+		} ] )
+		_adjust( 'Min. Block Height', 87, { viewport, unit: 'vh' }, 'assertComputedStyle', [ {
+			[ BLOCK_SELECTOR ]: {
+				[ `min-height` ]: '87vh',
+			},
+		} ] )
+
+		// Test Content Vertical Align.
+		const verticalAligns = [ 'flex-start', 'center', 'flex-end' ]
+		verticalAligns.forEach( align => {
+			_adjust( 'Content Vertical Align', align, { viewport }, 'assertComputedStyle', [ {
+				[ BLOCK_SELECTOR ]: {
+					[ `align-items` ]: align,
+				},
+			} ] )
+		} )
+
+		// Test Max Content Width.
+		_adjust( 'Max Content Width', 2303, { viewport }, 'assertComputedStyle', [ {
+			'.ugb-inner-block': {
+				[ `max-width` ]: '2303px',
+			},
+		} ] )
+		_adjust( 'Max Content Width', 78, { viewport, unit: '%' }, 'assertComputedStyle', [ {
+			'.ugb-inner-block': {
+				[ `max-width` ]: '78%',
+			},
+		} ] )
+
+		// Test Content Horizontal Align.
+		const horizontalAlign = [ 'flex-start', 'center', 'flex-end' ]
+		horizontalAlign.forEach( align => {
+			_adjust( 'Content Horizontal Align', align, { viewport }, 'assertComputedStyle', [ {
+				[ BLOCK_SELECTOR ]: {
+					[ `justify-content` ]: align,
+				},
+			} ] )
+		} )
+
+		const generateFourRangeControlAssertion = ( top, right, bottom, left, values = [], template = 'margin', unit = 'px' ) => {
+			const aligns = [ 'top', 'right', 'bottom', 'left' ]
+			const conditions = {
+				top, right, bottom, left,
+			}
+			const fourRangeControl = {}
+			const asserts = {}
+
+			aligns.forEach( ( align, index ) => {
+				fourRangeControl[ align ] = conditions[ align ] && values[ index ]
+				if ( conditions[ align ] ) {
+					asserts[ `${ template }-${ align }` ] = `${ values[ index ] }${ unit }`
+				}
+			} )
+
+			return [ compact( keys( fourRangeControl ).map( k => fourRangeControl[ k ] ) ), asserts ]
+		}
+
+		const units = [ 'em', 'px', '%' ]
+		units.forEach( unit => {
+			const values = unit === 'em' ? [ 3, 2, 1, 2 ] : [ 12, 65, 34, 23 ]
+			if ( unit !== 'em' ) {
+				 // Test Block Margins.
+				const [ margins, marginAsserts ] = generateFourRangeControlAssertion(
+					enableMarginTop,
+					enableMarginRight,
+					enableMarginBottom,
+					enableMarginLeft,
+					values,
+					'margin',
+					unit
+				)
+
+				_adjust( 'Block Margins', margins, { unit, viewport }, 'assertComputedStyle', [ { [ BLOCK_SELECTOR ]: marginAsserts } ] )
+			}
+
+			 // Test Block Paddings.
+			const [ paddings, paddingAsserts ] = generateFourRangeControlAssertion(
+				enablePaddingTop,
+				enablePaddingRight,
+				enablePaddingBottom,
+				enablePaddingLeft,
+				values,
+				'padding',
+				unit
+			)
+
+			_adjust( 'Block Paddings', paddings, { unit, viewport }, 'assertComputedStyle', [ { [ BLOCK_SELECTOR ]: paddingAsserts } ] )
+		} )
+
+		_collapse( 'Column / Container Spacing' )
+
+		// Test Column Gap.
+		_adjust( 'Column Gap', 24, { viewport }, 'assertComputedStyle', [ {
+			'.ugb-block-content': {
+				[ `column-gap` ]: '24px',
+			},
+		} ] )
+
+		// Test Column Vertical Align.
+		const columnVerticalAligns = [ 'flex-start', 'center', 'flex-end', 'stretch' ]
+		columnVerticalAligns.forEach( align => {
+			_adjust( 'Column Vertical Align', align, { viewport }, 'assertComputedStyle', [ {
+				'.ugb-block-content': {
+					[ `align-items` ]: align,
+				},
+			} ] )
+		} )
+
+		// Test Min. Column Height.
+		_adjust( 'Min. Column Height', 161, { viewport }, 'assertComputedStyle', [ {
+			'.ugb-block-content>*': {
+				[ `min-height` ]: '161px',
+			},
+		} ] )
+
+		// Test Content Vertical Align.
+		const contentVerticalAligns = [ 'flex-start', 'center', 'flex-end' ]
+		contentVerticalAligns.forEach( align => {
+			_adjust( 'Content Vertical Align', align, { viewport }, 'assertComputedStyle', [ {
+				'.ugb-block-content>*': {
+					[ `justify-content` ]: align,
+				},
+			} ] )
+		} )
+		callbacks[ viewport ]()
+	}
+
+	return () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( name )
+		const previewMode = [ 'Desktop', 'Tablet', 'Mobile' ]
+
+		previewMode.forEach( preview => {
+			_assertAdvancedTab( preview )
+		} )
+
+		_collapse( 'Responsive' )
+
+		previewMode.forEach( preview => {
+			cy.changePreviewMode( preview )
+			_adjust( `Hide on ${ preview }`, true, {}, 'assertComputedStyle', [ {
+				[ MAIN_SELECTOR ]: {
+					[ `display` ]: 'none',
+				},
+			}, {
+				assertBackend: false,
+			} ] )
+		} )
+
+		// TODO: Custom CSS
+		// TODO: HTML Anchor
+		// TODO: Additional CSS class(es)
+	}
+}
+
---
+++ cypress/support/modules.js
@@ -0,0 +1,448 @@
+/**
+ * Internal dependencies
+ */
+import { assertAligns } from './helpers'
+
+/**
+ * Assertion function for Block Title and Block Description.
+ *
+ * @param {Object} options
+ */
+export const assertBlockTitleDescription = ( options = {} ) => {
+	const {
+		viewport = 'Desktop',
+	} = options
+	switch ( viewport ) {
+		case 'Desktop':
+		// Test Block Title
+			cy.collapse( 'Block Title' )
+			cy.toggleStyle( 'Block Title' )
+			cy.adjust( 'Title HTML Tag', 'h2' )
+				.assertHtmlTag( '.ugb-block-title', 'h2' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Sans-Serif',
+				[ `Size` ]: 31,
+				[ `Weight` ]: '700',
+				[ `Transform` ]: 'uppercase',
+				[ `Line-Height` ]: {
+					value: 46,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 46 )
+			cy.adjust( 'Title Color', '#636363' )
+			cy.adjust( 'Max Width', 748 ).assertComputedStyle( {
+				'.ugb-block-title': {
+					[ `font-size` ]: '46px',
+					[ `font-weight` ]: '700',
+					[ `text-transform` ]: 'uppercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '748px',
+					[ `line-height` ]: '46px',
+				},
+			} )
+			cy.adjust( 'Horizontal Align', 'flex-start' ).assertComputedStyle( {
+				'.ugb-block-title': {
+					[ `margin-left` ]: '0px',
+				},
+			} )
+			cy.adjust( 'Horizontal Align', 'center' )
+			cy.adjust( 'Horizontal Align', 'flex-end' ).assertComputedStyle( {
+				'.ugb-block-title': {
+					[ `margin-right` ]: '0px',
+				},
+			} )
+
+			// Test Block Title Alignment
+			assertAligns( 'Text Align', '.ugb-block-title' )
+
+			// Test Block Description
+			cy.collapse( 'Block Description' )
+			cy.toggleStyle( 'Block Description' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Serif',
+				[ `Size` ]: 25,
+				[ `Weight` ]: '300',
+				[ `Transform` ]: 'lowercase',
+				[ `Line-Height` ]: {
+					value: 36,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 31 )
+			cy.adjust( 'Description Color', '#636363' )
+			cy.adjust( 'Max Width', 734 ).assertComputedStyle( {
+				'.ugb-block-description': {
+					[ `font-size` ]: '31px',
+					[ `font-weight` ]: '300',
+					[ `text-transform` ]: 'lowercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '734px',
+					[ `line-height` ]: '36px',
+				},
+			} )
+			cy.adjust( 'Horizontal Align', 'flex-start' ).assertComputedStyle( {
+				'.ugb-block-description': {
+					[ `margin-left` ]: '0px',
+				},
+			} )
+			cy.adjust( 'Horizontal Align', 'center' )
+			cy.adjust( 'Horizontal Align', 'flex-end' ).assertComputedStyle( {
+				'.ugb-block-description': {
+					[ `margin-right` ]: '0px',
+				},
+			} )
+
+			// Test Block Description Alignment
+			assertAligns( 'Text Align', '.ugb-block-description' )
+			break
+		case 'Tablet':
+		// Test Block Title
+			cy.collapse( 'Block Title' )
+			cy.toggleStyle( 'Block Title' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Sans-Serif',
+				[ `Size` ]: 31,
+				[ `Weight` ]: '700',
+				[ `Transform` ]: 'uppercase',
+				[ `Line-Height` ]: {
+					viewport: 'Tablet',
+					value: 42,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 46, { viewport: 'Tablet' } )
+			cy.adjust( 'Title Color', '#636363' )
+			cy.adjust( 'Max Width', 748, { viewport: 'Tablet' } ).assertComputedStyle( {
+				'.ugb-block-title': {
+					[ `font-size` ]: '46px',
+					[ `font-weight` ]: '700',
+					[ `text-transform` ]: 'uppercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '748px',
+					[ `line-height` ]: '42px',
+				},
+			} )
+
+			// Test Block Title Alignment
+			assertAligns( 'Text Align', '.ugb-block-title', { viewport: 'Tablet' } )
+
+			// Test Block Description
+			cy.collapse( 'Block Description' )
+			cy.toggleStyle( 'Block Description' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Sans-Serif',
+				[ `Size` ]: 21,
+				[ `Weight` ]: '700',
+				[ `Transform` ]: 'uppercase',
+				[ `Line-Height` ]: {
+					viewport: 'Tablet',
+					value: 38,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 36, { viewport: 'Tablet' } )
+			cy.adjust( 'Description Color', '#636363' )
+			cy.adjust( 'Max Width', 748, { viewport: 'Tablet' } ).assertComputedStyle( {
+				'.ugb-block-description': {
+					[ `font-size` ]: '36px',
+					[ `font-weight` ]: '700',
+					[ `text-transform` ]: 'uppercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '748px',
+					[ `line-height` ]: '38px',
+				},
+			} )
+
+			// Test Block Description Alignment
+			assertAligns( 'Text Align', '.ugb-block-description', { viewport: 'Tablet' } )
+			break
+		case 'Mobile':
+		// Test Block Title
+			cy.collapse( 'Block Title' )
+			cy.toggleStyle( 'Block Title' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Sans-Serif',
+				[ `Size` ]: 31,
+				[ `Weight` ]: '700',
+				[ `Transform` ]: 'uppercase',
+				[ `Line-Height` ]: {
+					viewport: 'Mobile',
+					value: 42,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 46, { viewport: 'Mobile' } )
+			cy.adjust( 'Title Color', '#636363' )
+			cy.adjust( 'Max Width', 748, { viewport: 'Mobile' } ).assertComputedStyle( {
+				'.ugb-block-title': {
+					[ `font-size` ]: '46px',
+					[ `font-weight` ]: '700',
+					[ `text-transform` ]: 'uppercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '748px',
+					[ `line-height` ]: '42px',
+				},
+			} )
+
+			// Test Block Title Alignment
+			assertAligns( 'Text Align', '.ugb-block-title', { viewport: 'Mobile' } )
+
+			// Test Block Description
+			cy.collapse( 'Block Description' )
+			cy.toggleStyle( 'Block Description' )
+			cy.adjust( 'Typography', {
+				[ `Font Family` ]: 'Sans-Serif',
+				[ `Size` ]: 21,
+				[ `Weight` ]: '700',
+				[ `Transform` ]: 'uppercase',
+				[ `Line-Height` ]: {
+					viewport: 'Mobile',
+					value: 38,
+					unit: 'px',
+				},
+				[ `Letter Spacing` ]: 1.3,
+			} )
+			cy.adjust( 'Size', 36, { viewport: 'Mobile' } )
+			cy.adjust( 'Description Color', '#636363' )
+			cy.adjust( 'Max Width', 748, { viewport: 'Mobile' } ).assertComputedStyle( {
+				'.ugb-block-description': {
+					[ `font-size` ]: '36px',
+					[ `font-weight` ]: '700',
+					[ `text-transform` ]: 'uppercase',
+					[ `letter-spacing` ]: '1.3px',
+					[ `color` ]: '#636363',
+					[ `max-width` ]: '748px',
+					[ `line-height` ]: '38px',
+				},
+			} )
+
+			// Test Block Description Alignment
+			assertAligns( 'Text Align', '.ugb-block-description', { viewport: 'Mobile' } )
+			break
+		default: break
+	}
+}
+
+/**
+ * Assertion function for Block Background.
+ *
+ * @param {Object} options
+ */
+export const assertBlockBackground = ( options = {} ) => {
+	const {
+		viewport = 'Desktop',
+	} = options
+
+	switch ( viewport ) {
+		case 'Desktop':
+		// Test Block Background options
+			cy.collapse( 'Block Background' )
+			cy.toggleStyle( 'Block Background' )
+			cy.adjust( 'No Paddings', true )
+			cy.adjust( 'Background Color', '#ffffff' )
+			cy.adjust( 'Background Color Opacity', 0.7 ).assertComputedStyle( {
+				'.ugb-icon': {
+					[ `background-color` ]: 'rgba(255, 255, 255, 0.7)',
+				},
+			} )
+			cy.adjust( 'Color Type', 'gradient' )
+			cy.adjust( 'Background Color #1', '#6d6d6d' )
+			cy.adjust( 'Background Color #2', '#cd2653' )
+			cy.adjust( 'Adv. Gradient Color Settings', {
+				[ `Gradient Direction (degrees)` ]: '180deg',
+				[ `Color 1 Location` ]: '11%',
+				[ `Color 2 Location` ]: '80%',
+				[ `Background Gradient Blend Mode` ]: 'multiply',
+			} ).assertComputedStyle( {
+				'.ugb-icon:before': {
+					[ `background-image` ]: 'linear-gradient(#6d6d6d 11%, #cd2653 80%)',
+					[ `mix-blend-mode` ]: 'multiply',
+				},
+			} )
+			// TODO: Add Background Image test
+			break
+		case 'Tablet':
+			// TODO: Add Background Image test
+			break
+		case 'Mobile':
+		// TODO: Add Background Image test
+			break
+		default: break
+	}
+}
+
+/**
+ * Assertion function for Top and Bottom Separator.
+ *
+ * @param {Object} options
+ */
+export const assertSeparators = ( options = {} ) => {
+	const {
+		viewport = 'Desktop',
+	} = options
+
+	const _adjust = ( adjustName, value, options = {}, assertionFunc, args = [] ) => {
+		cy.get( '.ugb-toggle-panel-body.is-opened', { log: false } )
+			.then( $panel => {
+				if ( $panel.text().includes( adjustName ) ) {
+					if ( args.length ) {
+						cy.adjust( adjustName, value, options )[ assertionFunc ]( ...args )
+					} else {
+						cy.adjust( adjustName, value, options )
+					}
+				}
+			} )
+	}
+
+	switch ( viewport ) {
+		case 'Desktop':
+		// Test Top Separator
+			cy.collapse( 'Top Separator' )
+			cy.toggleStyle( 'Top Separator' )
+			cy.adjust( 'Design', 'wave-2' )
+			cy.adjust( 'Color', '#000000' )
+			cy.adjust( 'Height', 237 )
+			cy.adjust( 'Width', 1.7 ).assertComputedStyle( {
+				'.ugb-top-separator svg': {
+					[ `fill` ]: '#000000',
+				},
+			} )
+			cy.adjust( 'Flip Horizontally', true )
+			cy.adjust( 'Flip Vertically', true )
+			cy.adjust( 'Shadow', false )
+			_adjust( 'Bring to Front', true, {}, 'assertComputedStyle', [ {
+				'.ugb-top-separator': {
+					'z-index': '6',
+				},
+			} ] )
+			cy.adjust( 'Separator Layer 2', {
+				[ `Color` ]: '#ffffff',
+				[ `Layer Height` ]: '1.16',
+				[ `Layer Width` ]: '1.9',
+				[ `Flip Horizontally` ]: true,
+				[ `Opacity` ]: '0.3',
+				[ `Mix Blend Mode` ]: 'overlay',
+			} ).assertComputedStyle( {
+				'.ugb-top-separator .ugb-separator__layer-2': {
+					[ `fill` ]: '#ffffff',
+					[ `transform` ]: 'matrix(-1.9, 0, 0, 1.16, 0, 0)',
+					[ `opacity` ]: '0.3',
+					[ `mix-blend-mode` ]: 'overlay',
+				},
+			} )
+
+			cy.adjust( 'Separator Layer 3', {
+				[ `Color` ]: '#6d6d6d',
+				[ `Layer Height` ]: '1.03',
+				[ `Layer Width` ]: '1.2',
+				[ `Flip Horizontally` ]: true,
+				[ `Opacity` ]: '0.8',
+			} ).assertComputedStyle( {
+				'.ugb-top-separator .ugb-separator__layer-3': {
+					[ `fill` ]: '#6d6d6d',
+					[ `transform` ]: 'matrix(-1.2, 0, 0, 1.03, 0, 0)',
+					[ `opacity` ]: '0.8',
+				},
+			} )
+
+			// Test Bottom Separator
+			cy.collapse( 'Bottom Separator' )
+			cy.toggleStyle( 'Bottom Separator' )
+			cy.adjust( 'Design', 'curve-3' )
+			cy.adjust( 'Color', '#f00069' )
+			cy.adjust( 'Height', 237 )
+			cy.adjust( 'Width', 1.7 ).assertComputedStyle( {
+				'.ugb-bottom-separator svg': {
+					[ `fill` ]: '#f00069',
+				},
+			} )
+			cy.adjust( 'Flip Horizontally', true )
+			cy.adjust( 'Flip Vertically', true )
+			cy.adjust( 'Shadow', false )
+			_adjust( 'Bring to Front', true, {}, 'assertComputedStyle', [ {
+				'.ugb-top-separator': {
+					'z-index': '6',
+				},
+			} ] )
+			cy.adjust( 'Separator Layer 2', {
+				[ `Color` ]: '#ffffff',
+				[ `Layer Height` ]: '1.16',
+				[ `Layer Width` ]: '1.9',
+				[ `Flip Horizontally` ]: true,
+				[ `Opacity` ]: '0.3',
+				[ `Mix Blend Mode` ]: 'saturation',
+			} ).assertComputedStyle( {
+				'.ugb-bottom-separator .ugb-separator__layer-2': {
+					[ `fill` ]: '#ffffff',
+					[ `transform` ]: 'matrix(-1.9, 0, 0, 1.16, 0, 0)',
+					[ `opacity` ]: '0.3',
+					[ `mix-blend-mode` ]: 'saturation',
+				},
+			} )
+
+			cy.adjust( 'Separator Layer 3', {
+				[ `Color` ]: '#6d6d6d',
+				[ `Layer Height` ]: '1.03',
+				[ `Layer Width` ]: '1.2',
+				[ `Flip Horizontally` ]: true,
+				[ `Opacity` ]: '0.8',
+			} ).assertComputedStyle( {
+				'.ugb-bottom-separator .ugb-separator__layer-3': {
+					[ `fill` ]: '#6d6d6d',
+					[ `transform` ]: 'matrix(-1.2, 0, 0, 1.03, 0, 0)',
+					[ `opacity` ]: '0.8',
+				},
+			} )
+			break
+		case 'Tablet':
+		// Test Top Separator
+			cy.collapse( 'Top Separator' )
+			cy.toggleStyle( 'Top Separator' )
+			cy.adjust( 'Height', 191, { viewport: 'Tablet' } ).assertComputedStyle( {
+				'.ugb-top-separator>.ugb-separator-wrapper': {
+					[ `height` ]: '191px',
+				},
+			} )
+
+			// Test Bottom Separator
+			cy.collapse( 'Bottom Separator' )
+			cy.toggleStyle( 'Bottom Separator' )
+			cy.adjust( 'Height', 150, { viewport: 'Tablet' } ).assertComputedStyle( {
+				'.ugb-bottom-separator>.ugb-separator-wrapper': {
+					[ `height` ]: '150px',
+				},
+			} )
+			break
+		case 'Mobile':
+		// Test Top Separator
+			cy.collapse( 'Top Separator' )
+			cy.toggleStyle( 'Top Separator' )
+			cy.adjust( 'Height', 191, { viewport: 'Mobile' } ).assertComputedStyle( {
+				'.ugb-top-separator>.ugb-separator-wrapper': {
+					[ `height` ]: '191px',
+				},
+			} )
+
+			// Test Bottom Separator
+			cy.collapse( 'Bottom Separator' )
+			cy.toggleStyle( 'Bottom Separator' )
+			cy.adjust( 'Height', 150, { viewport: 'Mobile' } ).assertComputedStyle( {
+				'.ugb-bottom-separator>.ugb-separator-wrapper': {
+					[ `height` ]: '150px',
+				},
+			} )
+			break
+		default: break
+	}
+}
