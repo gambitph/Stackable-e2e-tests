e283d5c0347dde4403626796940db71a0e753972
Nico Mendoza <blankartwp@gmail.com>
2021-02-04 15:16:54 +0800

    added style tests

--- cypress/integration/v2/blocks/icon-list.spec.js
+++ cypress/integration/v2/blocks/icon-list.spec.js
@@ -2,13 +2,18 @@
  * External dependencies
  */
 import {
-	assertBlockExist, blockErrorTest, switchDesigns, registerTests,
+	assertBlockExist, blockErrorTest, switchDesigns, registerTests, responsiveAssertHelper, assertAligns, assertBlockTitleDescription, assertBlockBackground, assertSeparators,
 } from '~stackable-e2e/helpers'
+
+const [ desktopStyle, tabletStyle, mobileStyle ] = responsiveAssertHelper( styleTab )
 
 describe( 'Icon List Block', registerTests( [
 	blockExist,
 	blockError,
 	switchDesign,
+	desktopStyle,
+	tabletStyle,
+	mobileStyle,
 ] ) )
 
 function blockExist() {
@@ -47,3 +52,112 @@ function switchDesign() {
 	] ) )
 }
 
+function styleTab( viewport, desktopOnly ) {
+	cy.setupWP()
+	cy.newPage()
+
+	cy.addBlock( 'ugb/icon-list' )
+
+	cy.openInspector( 'ugb/icon-list', 'Style' )
+	cy.collapse( 'General' )
+	cy.adjust( 'Columns', 4, { viewport } ).assertComputedStyle( {
+		'.ugb-icon-list ul': {
+			[ `columns` ]: 'auto 4',
+		},
+	}, { wait: 300 } )
+	desktopOnly( () => {
+		cy.adjust( 'Display as a grid (left to right & evenly spaced)', true )
+	} )
+	assertAligns( 'Align', '.ugb-inner-block', { viewport } )
+
+	desktopOnly( () => {
+		cy.collapse( 'Spacing' )
+		cy.adjust( 'List Gap', 25 ).assertComputedStyle( {
+			'ul li': {
+				[ `margin-bottom` ]: '25px',
+			},
+		} )
+	} )
+
+	cy.collapse( 'Icon' )
+	cy.waitFA()
+	cy.adjust( 'Icon', 'info' )
+	desktopOnly( () => {
+		cy.adjust( 'Icon Color', '#538fa9' )
+	} )
+	cy.adjust( 'Icon Size', 23, { viewport } ).assertComputedStyle( {
+		'li:before': {
+			[ `width` ]: '23px',
+			[ `height` ]: '23px',
+		},
+	} )
+	desktopOnly( () => {
+		cy.adjust( 'Icon Opacity', 0.6 )
+		cy.adjust( 'Icon Rotation', 128 ).assertComputedStyle( {
+			'li:before': {
+				[ `opacity` ]: '0.6',
+				[ `transform` ]: 'matrix(-0.615661, 0.788011, -0.788011, -0.615661, 0, 0)',
+			},
+		} )
+	} )
+
+	cy.collapse( 'List Text' )
+	cy.adjust( 'Typography', {
+		[ `Size` ]: {
+			viewport,
+			value: 50,
+		},
+		[ `Line-Height` ]: {
+			viewport,
+			unit: 'em',
+			value: 4,
+		},
+	} ).assertComputedStyle( {
+		li: {
+			[ `font-size` ]: '50px',
+			[ `line-height` ]: '4em',
+		},
+	}, { wait: 300 } )
+
+	cy.adjust( 'Typography', {
+		[ `Size` ]: {
+			viewport,
+			unit: 'em',
+			value: 7,
+		},
+		[ `Line-Height` ]: {
+			viewport,
+			unit: 'px',
+			value: 24,
+		},
+	} ).assertComputedStyle( {
+		li: {
+			[ `font-size` ]: '7em',
+			[ `line-height` ]: '24px',
+		},
+	}, { wait: 300 } )
+
+	desktopOnly( () => {
+		cy.adjust( 'Typography', {
+			[ `Weight` ]: '700',
+			[ `Transform` ]: 'lowercase',
+			[ `Letter Spacing` ]: 2.9,
+		} ).assertComputedStyle( {
+			li: {
+				[ `font-weight` ]: '700',
+				[ `text-transform` ]: 'lowercase',
+				[ `letter-spacing` ]: '2.9px',
+			},
+		} )
+
+		cy.adjust( 'Color', '#742f2f' ).assertComputedStyle( {
+			li: {
+				[ `color` ]: '#742f2f',
+			},
+		}, { wait: 300 } )
+	} )
+
+	assertBlockTitleDescription( { viewport } )
+	assertBlockBackground( '.ugb-icon-list', { viewport } )
+	assertSeparators( { viewport } )
+}
