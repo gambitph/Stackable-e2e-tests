c5d98332ea3d48d56eeb19b47358af737fdedc52
Nico Mendoza <blankartwp@gmail.com>
2021-02-05 11:14:03 +0800

    refactor modules

--- cypress/support/commands/attributes.js
+++ cypress/support/commands/attributes.js
@@ -1,14 +1,14 @@
 /**
  * Register functions to Cypress Commands.
  */
-Cypress.Commands.add( 'setBlockAttribute', setBlockAttributes )
+Cypress.Commands.add( 'setBlockAttribute', setBlockAttribute )
 
 /**
  * Command for setting attributes.
  *
  * @param {Object} attributes
  */
-export function setBlockAttributes( attributes = {} ) {
+export function setBlockAttribute( attributes = {} ) {
 	cy.window().then( win => {
 		const { clientId = '' } = win.wp.data.select( 'core/block-editor' ).getSelectedBlock() || {}
 		if ( clientId ) {
