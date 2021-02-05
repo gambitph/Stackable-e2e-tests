6bb9e477e334b9778890101e628aef20acac4e9a
Nico Mendoza <blankartwp@gmail.com>
2021-02-05 17:49:30 +0800

    add option to exclude test files

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,8 +3,28 @@
  * @type {Cypress.PluginConfig}
  */
 const webpack = require( '@cypress/webpack-preprocessor' )
+const path = require( 'path' )
+const { compact } = require( 'lodash' )
 
-module.exports = on => {
+// Force all files to be included for debugging.
+const FORCE_INCLUDE_ALL = false
+
+const INCLUDE_V2_TEST_FILES = [
+	// Blocks
+	'accordion',
+	'heading',
+	'blockqoute',
+	'text',
+	'cta',
+	'header',
+	'icon-list',
+	'divider',
+	'spacer',
+]
+
+const INCLUDE_V3_TEST_FILES = []
+
+module.exports = ( on, config ) => {
 	on( `task`, {
 		error( message ) {
 			console.error( message ) // eslint-disable-line no-console
@@ -16,4 +36,15 @@ module.exports = on => {
 		webpackOptions: require( '../../webpack.conf.js' ),
 		watchOptions: {},
 	} ) )
+
+	if ( ! FORCE_INCLUDE_ALL ) {
+		const includeV2TestFiles = INCLUDE_V2_TEST_FILES.length && path.join( config.integrationFolder, 'v2', '**', `!(?(${ INCLUDE_V2_TEST_FILES.join( '|' ) })).spec.js` )
+		const includeV3TestFiles = INCLUDE_V3_TEST_FILES.length && path.join( config.integrationFolder, 'v3', '**', `!(?(${ INCLUDE_V3_TEST_FILES.join( '|' ) })).spec.js` )
+
+		const includeTestFiles = compact( [ includeV2TestFiles, includeV3TestFiles ] ) || []
+		config.ignoreTestFiles = includeTestFiles.length > 1
+			? `{${ includeTestFiles.join( ',' ) }}`
+			: 		includeTestFiles.join( ',' )
+		return config
+	}
 }
