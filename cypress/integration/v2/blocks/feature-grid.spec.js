3fe427241566c25d42260c43023752c335632879
Nico Mendoza <blankartwp@gmail.com>
2021-02-08 12:14:36 +0800

    initial commit

--- cypress/integration/v2/blocks/feature-grid.spec.js
+++ cypress/integration/v2/blocks/feature-grid.spec.js
@@ -3,14 +3,30 @@
  * External dependencies
  */
 import {
-	assertBlockExist, blockErrorTest, switchDesigns, switchLayouts, assertAdvancedTab,
+	assertBlockExist, blockErrorTest, switchDesigns, switchLayouts, registerTests, assertAligns, responsiveAssertHelper,
 } from '~stackable-e2e/helpers'
 
-describe( 'Feature Grid Block', () => {
+const [ desktopStyle, tabletStyle, mobileStyle ] = responsiveAssertHelper( styleTab )
+
+describe( 'Feature Grid Block', registerTests( [
+	blockExist,
+	blockError,
+	switchLayout,
+	switchDesign,
+	desktopStyle,
+	tabletStyle,
+	mobileStyle,
+] ) )
+
+function blockExist() {
 	it( 'should show the block', assertBlockExist( 'ugb/feature-grid', '.ugb-feature-grid' ) )
+}
 
+function blockError() {
 	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'ugb/feature-grid' ) )
+}
 
+function switchLayout() {
 	it( 'should switch layout', switchLayouts( 'ugb/feature-grid', [
 		{ value: 'Basic', selector: '.ugb-feature-grid--design-basic' },
 		{ value: 'Plain', selector: '.ugb-feature-grid--design-plain' },
@@ -18,7 +34,9 @@ describe( 'Feature Grid Block', () => {
 		{ value: 'Large Mid', selector: '.ugb-feature-grid--design-large-mid' },
 		{ value: 'Zigzag', selector: '.ugb-feature-grid--design-zigzag' },
 	] ) )
+}
 
+function switchDesign() {
 	it( 'should switch design', switchDesigns( 'ugb/feature-grid', [
 		'Angled Feature Grid',
 		'Arch Feature Grid',
@@ -53,19 +71,103 @@ describe( 'Feature Grid Block', () => {
 		'Upland Feature Grid',
 		'Yule Feature Grid',
 	] ) )
+}
 
-	it( 'should adjust options inside advanced tab', assertAdvancedTab( 'ugb/feature-grid',
-		{},
-		// Desktop Test.
-		() => {
+function styleTab( viewport, desktopOnly ) {
+	cy.setupWP()
+	cy.newPage()
+	cy.addBlock( 'ugb/feature-grid' )
 
+	cy.openInspector( 'ugb/feature-grid', 'Style' )
+
+	cy.collapse( 'General' )
+	desktopOnly( () => {
+		cy.adjust( 'Columns', 4 )
+		cy.get( '.ugb-feature-grid__item4' ).should( 'exist' )
+	} )
+	assertAligns( 'Align', '.ugb-inner-block', { viewport } )
+
+	cy.collapse( 'Container' )
+	cy.setBlockAttribute( {
+		[ `column${ viewport === 'Desktop' ? '' : viewport }BackgroundMediaUrl` ]: Cypress.env( 'DUMMY_IMAGE_URL' ),
+		[ `image1Url` ]: Cypress.env( 'DUMMY_IMAGE_URL' ),
+		[ `image2Url` ]: Cypress.env( 'DUMMY_IMAGE_URL' ),
+		[ `image3Url` ]: Cypress.env( 'DUMMY_IMAGE_URL' ),
+		[ `image4Url` ]: Cypress.env( 'DUMMY_IMAGE_URL' ),
+	} )
+	desktopOnly( () => {
+		cy.adjust( 'Background', {
+			[ `Color Type` ]: 'gradient',
+			[ `Background Color #1` ]: '#ff5c5c',
+			[ `Background Color #2` ]: '#7bff5a',
+			[ `Adv. Gradient Color Settings` ]: {
+				[ `Gradient Direction (degrees)` ]: 160,
+				[ `Color 1 Location` ]: 28,
+				[ `Color 2 Location` ]: 75,
+				[ `Background Gradient Blend Mode` ]: 'hue',
+			},
+			[ `Background Media Tint Strength` ]: 6,
+			[ `Fixed Background` ]: true,
+			[ `Adv. Background Image Settings` ]: {
+				[ `Image Blend Mode` ]: 'exclusion',
+			},
+		} ).assertComputedStyle( {
+			'.ugb-feature-grid__item:before': {
+				[ `background-image` ]: 'linear-gradient(160deg, #ff5c5c 28%, #7bff5a 75%)',
+				[ `opacity` ]: '0.6',
+				[ `mix-blend-mode` ]: 'hue',
+			},
+			'.ugb-feature-grid__item': {
+				[ `background-color` ]: '#ff5c5c',
+				[ `background-attachment` ]: 'fixed',
+				[ `background-blend-mode` ]: 'exclusion',
+			},
+		} )
+	} )
+	cy.adjust( 'Background', {
+		[ `Adv. Background Image Settings` ]: {
+			[ `Image Position` ]: {
+				viewport,
+				value: 'center center',
+			},
+			[ `Image Repeat` ]: {
+				viewport,
+				value: 'repeat-x',
+			},
+			[ `Image Size` ]: {
+				viewport,
+				value: 'custom',
+			},
+			[ `Custom Size` ]: {
+				viewport,
+				value: 19,
+				unit: '%',
+			},
 		},
-		// Tablet Test.
-		() => {
-
+	} ).assertComputedStyle( {
+		'.ugb-feature-grid__item': {
+			[ `background-position` ]: '50% 50%',
+			[ `background-repeat` ]: 'repeat-x',
+			[ `background-size` ]: '19%',
 		},
-		// Mobile Test.
-		() => {
+	} )
+	desktopOnly( () => {
+		cy.adjust( 'Borders', 'solid' )
+		cy.adjust( 'Border Width', 4 )
+		cy.adjust( 'Border Color', '#a12222' )
+		cy.adjust( 'Border Radius', 26 ).assertComputedStyle( {
+			'.ugb-feature-grid__item': {
+				[ `border-style` ]: 'solid',
+				[ `border-top-width` ]: '4px',
+				[ `border-bottom-width` ]: '4px',
+				[ `border-left-width` ]: '4px',
+				[ `border-right-width` ]: '4px',
+				[ `border-color` ]: '#a12222',
+				[ `border-radius` ]: '26px',
+			},
+		} )
+		cy.adjust( 'Shadow / Outline', 7 )
+			.assertClassName( '.ugb-feature-grid__item', 'ugb--shadow-7' )
+	} )
+}
 
-		} ) )
-} )
