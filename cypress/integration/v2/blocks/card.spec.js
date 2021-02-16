b82930e8355cf0e90c6146ce0d1509bd4b9a00cd
Elaine Legaspi <elainelegaspi@Elaines-MacBook-Pro.local>
2021-02-16 11:36:48 +0800

    added card button tests

--- cypress/integration/v2/blocks/card.spec.js
+++ cypress/integration/v2/blocks/card.spec.js
@@ -337,6 +337,105 @@ function styleTab( viewport, desktopOnly ) {
 	assertTypography( '.ugb-card__description', { viewport } )
 	assertAligns( 'Align', '.ugb-card__description', { viewport } )
 
+	// Test Button options
+	cy.collapse( 'Button' )
+	desktopOnly( () => {
+		cy.adjust( 'Button Color Type', 'gradient' )
+		cy.adjust( 'Button Color #1', '#a13939' )
+		cy.adjust( 'Button Color #2', '#4e59d4' )
+		cy.adjust( 'Gradient Direction (degrees)', 138 )
+		cy.adjust( 'Text Color', '#ffa03b' )
+		cy.adjust( 'Hover Effect', 'scale' )
+			.assertClassName( '.ugb-button', 'ugb--hover-effect-scale' )
+		cy.adjust( 'Hover Opacity', 0.6 )
+		cy.adjust( 'Hover Colors', {
+			[ `Button Color #1` ]: '#bd8b8b',
+			[ `Button Color #2` ]: '#3fa35b',
+			[ `Gradient Direction (degrees)` ]: 72,
+			[ `Text Color` ]: '#80194d',
+		} )
+		cy.adjust( 'Typography', {
+			[ `Size` ]: 50,
+			[ `Weight` ]: '700',
+			[ `Transform` ]: 'lowercase',
+			[ `Letter Spacing` ]: 2.9,
+		} )
+		cy.adjust( 'Button Size', 'large' )
+			.assertClassName( '.ugb-button', 'ugb-button--size-large' )
+		cy.adjust( 'Border Radius', 40 )
+		cy.adjust( 'Vertical Padding', 15 )
+		cy.adjust( 'Horizontal Padding', 43 )
+		cy.adjust( 'Shadow', 4 )
+		cy.adjust( 'Opacity', 0.6 ).assertComputedStyle( {
+			'.ugb-button .ugb-button--inner': {
+				[ `font-size` ]: '50px',
+				[ `font-weight` ]: '700',
+				[ `text-transform` ]: 'lowercase',
+				[ `letter-spacing` ]: '2.9px',
+			},
+			'.ugb-button': {
+				[ `background-color` ]: '#a13939',
+				[ `background-image` ]: 'linear-gradient(138deg, #a13939, #4e59d4)',
+				[ `padding-top` ]: '15px',
+				[ `padding-right` ]: '43px',
+				[ `padding-bottom` ]: '15px',
+				[ `padding-left` ]: '43px',
+				[ `opacity` ]: '0.6',
+				[ `border-radius` ]: '40px',
+			},
+		} )
+		cy.adjust( 'Typography', {
+			[ `Size` ]: {
+				unit: 'em',
+				value: 7,
+			},
+		} ).assertComputedStyle( {
+			'.ugb-button .ugb-button--inner': {
+				[ `font-size` ]: '7em',
+			},
+		} )
+		cy.waitFA()
+		cy.adjust( 'Icon', 'info' )
+		cy.adjust( 'Adv. Icon Settings', {
+			[ `Icon Size` ]: 41,
+			[ `Icon Spacing` ]: 25,
+		} ).assertComputedStyle( {
+			'.ugb-button svg': {
+				[ `height` ]: '41px',
+				[ `width` ]: '41px',
+				[ `margin-right` ]: '25px',
+			},
+		} )
+	} )
+
+	const tabletMobileViewports = [ 'Tablet', 'Mobile' ]
+	if ( tabletMobileViewports.some( _viewport => _viewport === viewport ) ) {
+		cy.adjust( 'Typography', {
+			[ `Size` ]: {
+				viewport,
+				value: 50,
+			},
+		} ).assertComputedStyle( {
+			'.ugb-button .ugb-button--inner': {
+				[ `font-size` ]: '50px',
+			},
+		} )
+
+		cy.adjust( 'Typography', {
+			[ `Size` ]: {
+				viewport,
+				unit: 'em',
+				value: 7,
+			},
+		} ).assertComputedStyle( {
+			'.ugb-button .ugb-button--inner': {
+				[ `font-size` ]: '7em',
+			},
+		} )
+	}
+
+	assertAligns( 'Align', '.ugb-button-container', { viewport } )
+
 	// Test Effects option
 	desktopOnly( () => {
 		cy.collapse( 'Effects' )
