03107d4fc3158e723b6cddc94bbcb6ed1c1dc1c3
legaspielaine <elaineelegaspi@gmail.com>
2021-03-24 13:24:10 +0800

    commit temporary change

--- cypress/support/gutenberg/commands/index.js
+++ cypress/support/gutenberg/commands/index.js
@@ -1,4 +1,23 @@
 /**
+ * Overwrite Cypress Commands
+ */
+Cypress.Commands.overwrite( 'visit', ( originalFn, url, options ) => {
+	cy.log( 'remove transitions !!' )
+	cy.getPostUrls().then( ( { editorUrl } ) => {
+		// Check if the url matches the editor, and new page URL
+		if ( url === editorUrl ||
+            url === '/wp-admin/post-new.php?post_type=page' ) {
+			// Remove the transitions
+			options.onLoad = () => {
+				cy.removeGlobalCssTransitions()
+			}
+		}
+	} )
+
+	return originalFn( url, options )
+} )
+
+/**
  * Custom commands for Gutenberg.
  */
 import './assertions'
