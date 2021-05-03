00b81b2c14d638deb38788a094c564211515338b
legaspielaine <elaineelegaspi@gmail.com>
2021-05-03 18:43:41 +0800

    initial test for global typography

--- cypress/integration/v2/features/global-settings.js
+++ cypress/integration/v2/features/global-settings.js
@@ -1,9 +1,23 @@
 /**
  * External dependencies
  */
+import { range } from 'lodash'
 import { registerBlockSnapshots } from '~gutenberg-e2e/plugins'
+import {
+	responsiveAssertHelper,
+	registerTests,
+} from '~stackable-e2e/helpers'
 
-describe( 'Global Settings', () => {
+const [ desktopGlobal, tabletGlobal, mobileGlobal ] = responsiveAssertHelper( globalTypography )
+
+describe( 'Global Settings', registerTests( [
+	globalColors,
+	desktopGlobal,
+	tabletGlobal,
+	mobileGlobal,
+] ) )
+
+function globalColors() {
 	it( 'should adjust Stackable Global Colors', () => {
 		cy.setupWP()
 		cy.newPage()
@@ -58,6 +72,95 @@ describe( 'Global Settings', () => {
 				} )
 		} )
 
+		// Delete Global colors
+		colors.forEach( val => {
+			cy.deleteGlobalColor( val.name )
+		} )
+
 		cardBlock.assertFrontendStyles()
 	} )
-} )
+}
+
+function globalTypography( viewport ) {
+	it( 'should adjust Stackable Global Typography', () => {
+		cy.setupWP()
+		cy.newPage()
+		range( 1, 7 ).forEach( idx => {
+			cy.addBlock( 'ugb/heading' )
+			cy.typeBlock( 'ugb/heading', '.ugb-heading__title', `Heading ${ idx }`, idx - 1 )
+			cy.typeBlock( 'ugb/heading', '.ugb-heading__subtitle', `This is paragraph ${ idx }`, idx - 1 )
+			cy.openInspector( 'ugb/heading', 'Style', `Heading ${ idx }` )
+			cy.collapse( 'Title' )
+			cy.adjust( 'Title HTML Tag', `h${ idx }` )
+		} )
+
+		// Adjust Global Typography settings
+		cy.adjustGlobalTypography( 'h1', {
+			'Font Family': 'Abel',
+			'Size': {
+				value: 92,
+				unit: 'px',
+				viewport,
+			},
+			'Weight': 'bold',
+			'Transform': 'uppercase',
+			'Line-Height': {
+				value: 1.3,
+				unit: 'em',
+				viewport,
+			},
+			'Letter Spacing': 2.1,
+		} )
+
+		cy.adjustGlobalTypography( 'h2', {
+			'Font Family': 'Aclonica',
+			'Size': {
+				value: 66,
+				unit: 'px',
+				viewport,
+			},
+			'Weight': '900',
+			'Transform': 'capitalize',
+			'Line-Height': {
+				value: 1.2,
+				unit: 'em',
+				viewport,
+			},
+			'Letter Spacing': 1.1,
+		} )
+
+		cy.adjustGlobalTypography( 'h3', {
+			'Font Family': 'ABeeZee',
+			'Size': {
+				value: 50,
+				unit: 'px',
+				viewport,
+			},
+			'Weight': '100',
+			'Transform': 'lowercase',
+			'Line-Height': {
+				value: 1.1,
+				unit: 'em',
+				viewport,
+			},
+			'Letter Spacing': 1.5,
+		} )
+
+		cy.adjustGlobalTypography( 'h4', {
+			'Font Family': 'ABeeZee',
+			'Size': {
+				value: 38,
+				unit: 'px',
+				viewport,
+			},
+			'Weight': '300',
+			'Transform': 'uppercase',
+			'Line-Height': {
+				value: 2.7,
+				unit: 'em',
+				viewport,
+			},
+			'Letter Spacing': 1.8,
+		} )
+	} )
+}
