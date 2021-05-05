5235e2f740656f56b8ec438b9710f8d811206e13
legaspielaine <elaineelegaspi@gmail.com>
2021-05-05 15:16:34 +0800

    initial test for global settings

---
+++ cypress/integration/v2/features/global-colors.js
@@ -0,0 +1,69 @@
+/**
+ * External dependencies
+ */
+import { registerBlockSnapshots } from '~gutenberg-e2e/plugins'
+
+describe( 'Global Settings', () => {
+	it( 'should adjust Stackable Global Colors', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'ugb/card' ).as( 'cardBlock' )
+		const cardBlock = registerBlockSnapshots( 'cardBlock' )
+
+		const colors = [
+			{
+				name: 'Custom Color Red',
+				color: '#ff0000',
+			},
+			{
+				name: 'Aqua',
+				color: '#3fcee8',
+			},
+			{
+				name: 'Yellow Sun',
+				color: '#f2e374',
+			},
+			{
+				name: 'Stackable Pink',
+				color: '#f00069',
+			} ]
+
+		// Add Global colors
+		colors.forEach( val => {
+			cy.addGlobalColor( {
+				name: val.name,
+				color: val.color,
+			} )
+		} )
+		cy.adjust( 'Use only Stackable colors', true )
+
+		// Check if the Global colors are added in blocks
+		cy.openInspector( 'ugb/card', 'Style' )
+		cy.collapse( 'Title' )
+
+		colors.forEach( ( val, idx ) => {
+			cy
+				.get( '.editor-color-palette-control__color-palette' )
+				.find( '.components-circular-option-picker__option-wrapper' )
+				.eq( idx )
+				.find( 'button' )
+				.click( { force: true } )
+
+			cy
+				.selectBlock( 'ugb/card' )
+				.assertComputedStyle( {
+					'.ugb-card__title': {
+						'color': val.color,
+					},
+				} )
+		} )
+
+		// Delete Global colors
+		colors.forEach( val => {
+			cy.deleteGlobalColor( val.name )
+		} )
+		cy.adjust( 'Use only Stackable colors', false )
+
+		cardBlock.assertFrontendStyles()
+	} )
+} )
---
+++ cypress/integration/v2/features/global-typography.js
@@ -0,0 +1,154 @@
+/**
+ * External dependencies
+ */
+import { range } from 'lodash'
+import {
+	responsiveAssertHelper,
+	registerTests,
+} from '~stackable-e2e/helpers'
+
+const [ desktopGlobal, tabletGlobal, mobileGlobal ] = responsiveAssertHelper( globalTypography, { tab: 'Global Typography' } )
+
+describe( 'Global Settings', registerTests( [
+	desktopGlobal,
+	tabletGlobal,
+	mobileGlobal,
+] ) )
+
+function globalTypography( viewport, desktopOnly ) {
+	cy.setupWP()
+	cy.newPage()
+
+	const globalTypo = [
+		{
+			tag: 'h1',
+			font: 'Abel',
+			size: 92,
+			weight: 'bold',
+			transform: 'uppercase',
+			lineHeight: 1.3,
+			letterSpacing: 2.1,
+		},
+		{
+			tag: 'h2',
+			font: 'Aclonica',
+			size: 66,
+			weight: '900',
+			transform: 'capitalize',
+			lineHeight: 1.2,
+			letterSpacing: 1.1,
+		},
+		{
+			tag: 'h3',
+			font: 'ABeeZee',
+			size: 50,
+			weight: '100',
+			transform: 'lowercase',
+			lineHeight: 1.1,
+			letterSpacing: 1.5,
+		},
+		{
+			tag: 'h4',
+			font: 'Acme',
+			size: 38,
+			weight: '300',
+			transform: 'uppercase',
+			lineHeight: 2.7,
+			letterSpacing: 1.8,
+		},
+		{
+			tag: 'h5',
+			font: 'Akronim',
+			size: 24,
+			weight: '500',
+			transform: 'capitalize',
+			lineHeight: 2.1,
+			letterSpacing: 3.9,
+		},
+		{
+			tag: 'h6',
+			font: 'Actor',
+			size: 16,
+			weight: '600',
+			transform: 'uppercase',
+			lineHeight: 1.4,
+			letterSpacing: 3.5,
+		},
+		{
+			tag: 'p',
+			font: 'Alice',
+			size: 15,
+			weight: '200',
+			transform: 'lowercase',
+			lineHeight: 1.3,
+			letterSpacing: 0.1,
+		},
+	]
+
+	cy.addBlock( 'ugb/heading' )
+	cy.typeBlock( 'ugb/heading', '.ugb-heading__title', 'Heading' )
+	cy.typeBlock( 'ugb/heading', '.ugb-heading__subtitle', 'This is paragraph' )
+
+	// Adjust Global Typography settings
+	desktopOnly( () => {
+		globalTypo.forEach( val => {
+			cy.adjustGlobalTypography( val.tag, {
+				'Font Family': val.font,
+				'Size': {
+					value: val.size,
+					unit: 'px',
+				},
+				'Weight': val.weight,
+				'Transform': val.transform,
+				'Line-Height': {
+					value: val.lineHeight,
+					unit: 'em',
+				},
+				'Letter Spacing': val.letterSpacing,
+			} )
+
+			cy.openInspector( 'ugb/heading', 'Style' )
+			cy.collapse( 'Title' )
+			cy.adjust( 'Title HTML Tag', val.tag ).assertComputedStyle( {
+				'.ugb-heading__title': {
+					'font-family': `${ val.font }, sans-serif`,
+					'font-size': `${ val.size }px`,
+					'font-weight': val.weight,
+					'text-transform': val.transform,
+					'line-height': `${ val.lineHeight }em`,
+					'letter-spacing': `${ val.letterSpacing }px`,
+				},
+			} )
+		} )
+	} )
+
+	const emFontSize = [ 4.2, 4.1, 3.9, 3.8, 3.7, 3.6, 3.5 ]
+	const pxLineHeight = [ 64, 58, 54, 48, 44, 38, 34 ]
+
+	range( 1, 8 ).forEach( idx => {
+		cy.adjustGlobalTypography( globalTypo[ idx - 1 ].tag, {
+			'Size': {
+				value: emFontSize[ idx - 1 ],
+				unit: 'em',
+				viewport,
+			},
+			'Line-Height': {
+				value: pxLineHeight[ idx - 1 ],
+				unit: 'px',
+				viewport,
+			},
+		} )
+
+		cy.openInspector( 'ugb/heading', 'Style' )
+		cy.collapse( 'Title' )
+		cy.adjust( 'Title HTML Tag', globalTypo[ idx - 1 ].tag ).assertComputedStyle( {
+			'.ugb-heading__title': {
+				'font-size': `${ emFontSize[ idx - 1 ] }em`,
+				'line-height': `${ pxLineHeight[ idx - 1 ] }px`,
+			},
+		} )
+
+		// Reset Global Typography settings
+		cy.resetGlobalTypography( globalTypo[ idx - 1 ].tag )
+	} )
+}
