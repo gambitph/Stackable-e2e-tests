599f87a6a3982e380723bc0161d285504915e8d6
legaspielaine <elaineelegaspi@gmail.com>
2021-06-24 18:00:19 +0800

    added more editor commands

--- cypress/support/gutenberg/commands/editor.js
+++ cypress/support/gutenberg/commands/editor.js
@@ -19,6 +19,8 @@ Cypress.Commands.add( 'savePost', savePost )
 Cypress.Commands.add( 'wp', wp )
 Cypress.Commands.add( 'typePostTitle', typePostTitle )
 Cypress.Commands.add( 'getPostData', getPostData )
+Cypress.Commands.add( 'addFeaturedImage', addFeaturedImage )
+Cypress.Commands.add( 'addPostExcerpt', addPostExcerpt )
 
 /**
  * Command for opening a new page in gutenberg editor.
@@ -215,6 +217,7 @@ export function typePostTitle( title ) {
 		.get( '.edit-post-visual-editor__post-title-wrapper' )
 		.find( 'textarea.editor-post-title__input' )
 		.type( `{selectall}${ title }`, { force: true } )
+	cy.savePost()
 }
 
 /**
@@ -227,4 +230,61 @@ export function getPostData() {
 			resolve( data )
 		} )
 	} )
+}
+
+/**
+ * Command for adding a featured image to a post.
+ */
+export function addFeaturedImage() {
+	cy.getPostUrls().then( ( { editorUrl } ) => {
+		// Save the current post as we're going to register a post.
+		cy.savePost()
+		// Register one post to be able to add an image to media library.
+		cy.registerPosts( { numOfPosts: 1 } )
+		cy.visit( editorUrl )
+		cy.openSidebar( 'Settings' )
+		cy
+			.get( '.edit-post-sidebar__panel-tabs' )
+			.find( 'li:first-child button.edit-post-sidebar__panel-tab' )
+			.click( { force: true } )
+		cy.collapse( 'Featured image' )
+		cy
+			.get( '.editor-post-featured-image' )
+			.contains( containsRegExp( 'Set featured image' ) )
+			.click( { force: true } )
+
+		const selector = () => cy
+			.get( '.media-modal-content' )
+
+		selector()
+			.find( '.media-frame-router' )
+			.contains( containsRegExp( 'Media Library' ) )
+			.click( { force: true } )
+
+		selector()
+			.find( 'ul.attachments li.attachment' )
+			.eq( 0 )
+			.click( { force: true } )
+
+		selector()
+			.find( 'button.media-button-select:contains(Set featured image)' )
+			.click( { force: true } )
+	} )
+}
+
+/**
+ * Command for adding an excerpt to a post.
+ *
+ * @param {string} text
+ */
+export function addPostExcerpt( text ) {
+	cy.openSidebar( 'Settings' )
+	cy
+		.get( '.edit-post-sidebar__panel-tabs' )
+		.find( 'li:first-child button.edit-post-sidebar__panel-tab' )
+		.click( { force: true } )
+	cy.collapse( 'Excerpt' )
+	cy
+		.get( '.editor-post-excerpt textarea.components-textarea-control__input' )
+		.type( `{selectall}${ text }`, { force: true } )
 }
