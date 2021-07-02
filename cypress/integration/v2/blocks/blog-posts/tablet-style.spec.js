bbfc7d506e6cdf8fe712f92597b885311e4fcb31
legaspielaine <elaineelegaspi@gmail.com>
2021-07-02 16:19:41 +0800

    Update cypress/integration/v2/blocks/blog-posts/tablet-style.spec.js
    Co-authored-by: blankart <blankartwp@gmail.com>

--- cypress/integration/v2/blocks/blog-posts/tablet-style.spec.js
+++ cypress/integration/v2/blocks/blog-posts/tablet-style.spec.js
@@ -2,5 +2,5 @@
  * External dependencies
  */
 import { registerTests } from '~stackable-e2e/helpers'
-import { tabletStyle } from './index'
+import { tabletStyle } from './'
 describe( 'Blog Posts Block ( Tablet style )', registerTests( [ tabletStyle ] ) )
