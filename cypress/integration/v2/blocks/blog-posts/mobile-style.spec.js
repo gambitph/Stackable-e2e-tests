d00c65f0939f3230c02c69d680455773b19c5278
legaspielaine <elaineelegaspi@gmail.com>
2021-07-02 16:19:16 +0800

    Update cypress/integration/v2/blocks/blog-posts/mobile-style.spec.js
    Co-authored-by: blankart <blankartwp@gmail.com>

--- cypress/integration/v2/blocks/blog-posts/mobile-style.spec.js
+++ cypress/integration/v2/blocks/blog-posts/mobile-style.spec.js
@@ -2,5 +2,5 @@
  * External dependencies
  */
 import { registerTests } from '~stackable-e2e/helpers'
-import { mobileStyle } from './index'
+import { mobileStyle } from './'
 describe( 'Blog Posts Block ( Mobile style )', registerTests( [ mobileStyle ] ) )
