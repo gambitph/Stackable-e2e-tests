7bef7784fc77ffa413c5e5bbabe6c3cfeef2dcc5
legaspielaine <elaineelegaspi@gmail.com>
2021-07-02 16:19:30 +0800

    Update cypress/integration/v2/blocks/blog-posts/desktop-style.spec.js
    Co-authored-by: blankart <blankartwp@gmail.com>

--- cypress/integration/v2/blocks/blog-posts/desktop-style.spec.js
+++ cypress/integration/v2/blocks/blog-posts/desktop-style.spec.js
@@ -2,5 +2,5 @@
  * External dependencies
  */
 import { registerTests } from '~stackable-e2e/helpers'
-import { desktopStyle } from './index'
+import { desktopStyle } from './'
 describe( 'Blog Posts Block ( Desktop style )', registerTests( [ desktopStyle ] ) )
