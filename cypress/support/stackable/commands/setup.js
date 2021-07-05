c3caeb0809efe3ef9076b1e3ac50b27b61f8b3b4
legaspielaine <elaineelegaspi@gmail.com>
2021-07-05 16:34:38 +0800

    Added a comment in stackable setup

--- cypress/support/stackable/commands/setup.js
+++ cypress/support/stackable/commands/setup.js
@@ -5,6 +5,7 @@ Cypress.Commands.overwrite( 'setupWP', ( originalFn, ...args ) => {
 	const optionsToPass = args.length === 2 ? args.pop() : {}
 
 	optionsToPass.pluginSetupCallback = () => {
+		// Enable optimization setting when calling setupWP.
 		cy.enableOptimization()
 	}
 
