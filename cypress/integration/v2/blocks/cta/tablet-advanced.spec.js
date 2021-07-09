671272e930dc0fc480d411b5d3ec7a95552794b7
legaspielaine <elaineelegaspi@gmail.com>
2021-07-09 13:42:38 +0800

    separated spec files for cta

---
+++ cypress/integration/v2/blocks/cta/tablet-advanced.spec.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { registerTests } from '~stackable-e2e/helpers'
+
+/**
+ * Internal dependencies
+ */
+import { tabletAdvanced } from '.'
+
+describe( 'Call To Action Block ( Tablet Advanced )', registerTests( [ tabletAdvanced ] ) )
