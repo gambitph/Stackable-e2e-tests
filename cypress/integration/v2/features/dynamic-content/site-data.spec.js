e6c0dc01f3201589fca34c712ca086848c6029d0
sethdg <sethdgabon@gmail.com>
2021-07-15 21:24:05 +1200

    revised function assertSiteData

--- cypress/integration/v2/features/dynamic-content/site-data.spec.js
+++ cypress/integration/v2/features/dynamic-content/site-data.spec.js
@@ -3,70 +3,70 @@
  * External dependencies
  */
 import { registerTests } from '~stackable-e2e/helpers'
+import { containsRegExp } from '~common/util'
 describe( 'Dynamic Content - Site', registerTests( [
-	  adjustSiteData, matchSiteData,
+	  assertSiteData,
 ] ) )
 
+const selector = () => cy.get( '.ugb-cta__title' )
 const fields = {
 	 'Site Title': 'title',
 	 'Site Tagline': 'tagline',
 	 'Site URL': 'url',
 }
+const assertValue = value => selector().contains( containsRegExp( value ) ).should( 'exist' )
 
-function adjustSiteData() {
-	 it( 'should adjust the site data', () => {
-		 cy.loginAdmin()
-		 //find and click customize
-		 cy
-			 .contains( 'Customize Your Site' )
-			 .click( { force: true } )
-		 //find 'site identity' and edit fields for site title, tagline
-		 cy
-			 .contains( 'Site Identity' )
-			 .click( { force: true } )
-
-		 cy
-			 .get( '[id$=-input-blogname]' ).clear( { force: true } )
-			 .type( 'Test Site Title', { force: true } )
-			 .get( '[id$=-input-blogdescription]' ).clear( { force: true } )
-			 .type( 'Test Site Tagline', { force: true } )
-
-		 cy
-			 .contains( 'Publish' )
-			 .click( { force: true } )
-
-		 cy.get( 'a[href*="e2etest"]' ).first().click() //exit out of customize site
-
-		 cy.wait( 5000 )
+const testString = [ 'Test Site Title', 'Test Site Tagline' ]
+const adjustSiteField = ( fieldName, fieldOptions = {} ) => {
+	 cy.adjustDynamicContent( 'ugb/cta', 0, '.ugb-cta__title', {
+		 source: 'Site',
+		 fieldName,
+		 fieldOptions,
 	 } )
 }
 
-function matchSiteData() {
-	 it( 'should match the site data', () => {
+function assertSiteData() {
+	 it( 'should match and assert adjusted Site Data', () => {
 		 cy.setupWP()
+		 cy.editSiteTitle( testString[ 0 ] )
+		 cy.editSiteTagline( testString[ 1 ] )
 
 		 Object.keys( fields ).forEach( fieldName => {
-			 //cy.wait( 5000 )
 			 cy.newPost()
-			 cy.typePostTitle( `${ fieldName } test` )
 			 cy.addBlock( 'ugb/cta' )
+			 cy.typePostTitle( `${ fieldName } test` )
 
-			 cy.adjustDynamicContent( 'ugb/cta', 0, '.ugb-cta__title', {
-				 source: 'Site',
-				 fieldName,
-			 } )
-			 cy.savePost()
+			 if ( fieldName === 'Site Title' ) {
+				 adjustSiteField( fieldName )
+				 assertValue( testString[ 0 ] )
+			 }
+			 if ( fieldName === 'Site Tagline' ) {
+				 adjustSiteField( fieldName )
+				 assertValue( testString[ 1 ] )
+			 }
+			 if ( fieldName === 'Site URL' ) {
+				 adjustSiteField( fieldName, { 'Show as link': true } )
+			 }
 		 } )
 	 } )
 }
 
 /*
-	 errors:
-		 - does not update values after site data is adjusted
 	 to do:
-		 - assert dynamic content matches corresponding site data
+	 - edit Site fields
+	 - adjust Dynamic Content; assert data matches edited Site fields
 		 - assert that DC changes in frontend once site data also changes
-		 - assert that if the field is empty, placeholder is present in backend while also empty in frontend
-		 - define test stringsand assert on those variables
+	 - assert that if the field is empty, placeholder is present in backend while also empty in frontend
+
+	 steps:
+	 [x]declare stringObj = (Title: This is a site title, Tagline: This is a site tagline)
+	 [x]const adjustSiteField = call adjustDynamicContent passing CTA block
+
+	 function assertSiteData
+		 [x]edit fieldName (if Site Title or Tagline)
+		 [x] adjustSiteField
+		 [x]assert Data matches
+		 - assert Data is seen (access previewURL)
+
  */
 
