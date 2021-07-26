cb781610b84912a0491b0db5657be9675a1404ba
sethdg <sethdgabon@gmail.com>
2021-07-26 17:07:30 +1200

    matchsitedata draft code

--- cypress/integration/v2/features/dynamic-content/latest-post.spec.js
+++ cypress/integration/v2/features/dynamic-content/latest-post.spec.js
@@ -10,14 +10,28 @@ describe( 'Dynamic Content - Latest Post', registerTests( [
 	adjustFieldValues,
 ] ) )
 
-/*
 const fields = {
-    'Post Title',
-    'Po'
- }
- */
+	'Post Title': 'title',
+	'Post URL': 'link',
+	'Post ID': 'id',
+	'Post Slug': 'slug',
+	'Post Excerpt': 'excerpt',
+	'Post Date': 'date',
+	'Post Date GMT': 'date_gmt',
+	'Post Modified': 'modified',
+	'Post Modified GMT': 'modified_gmt',
+	'Post Type': 'type',
+	'Post Status': 'status',
+	'Author Name': 'name',
+	'Author ID': 'author',
+	'Author Posts URL': 'url',
+	'Comment Number': 'comments_num',
+	'Comment Status': 'comment_status',
+	'Featured Image URL': 'featured_image_urls',
+}
+
 //const nthLatest = [ '' ]
-/*
+
 const adjustField = ( fieldName, fieldOptions = {} ) => {
 	cy.adjustDynamicContent( 'ugb/cta', 0, '.ugb-cta__title', {
 		source: 'Latest Post',
@@ -25,10 +39,17 @@ const adjustField = ( fieldName, fieldOptions = {} ) => {
 		fieldOptions,
 	} )
 }
-*/
+
 function matchSiteData() {
 	it( 'should match dynamic content in site fields', () => {
 		cy.setupWP()
+		Object.keys( fields ).forEach( fieldName => {
+			cy.newPost()
+			cy.typePostTitle( `${ fieldName } test` )
+
+			cy.addBlock( 'ugb/cta' )
+			adjustField( fieldName ) //add fieldoptions
+		} )
 	} )
 }
 
@@ -37,10 +58,29 @@ function adjustFieldOptions() {
 		cy.setupWP()
 	} )
 }
-//site URL was excluded for this test, since it cannot be changed
+
 function adjustFieldValues() {
 	it( 'should assert changes to the field values', () => {
 		cy.setupWP()
 	} )
 }
 
+/*
+    testing flow:
+    1. add placeholder posts for latest posts (1st to 3rd)
+        must have:
+        - post title
+        - post URL,
+        - post id,
+        - post slug
+        - post excerpt,
+        - post date,
+        - post date GMT,
+        - post modified
+        - post modified GMT,
+        - post type,
+        - post status
+    2. display and assert dynamic content function
+    3. publish pages
+    4. test latest post DC function per nth post
+*/
