44fa3975644bc708d19fc8b91f895bded9ab297d
sethdg <sethdgabon@gmail.com>
2021-07-29 19:51:31 +1200

    testing addtoTest function [skip ci]

--- cypress/integration/v2/features/dynamic-content/latest-post.spec.js
+++ cypress/integration/v2/features/dynamic-content/latest-post.spec.js
@@ -3,38 +3,17 @@
  * External dependencies
  */
 import { registerTests } from '~stackable-e2e/helpers'
-import { range, uniqueId } from 'lodash'
+import {
+	first, uniqueId,
+} from 'lodash'
 
 describe( 'Dynamic Content - Latest Post', registerTests( [
-	setFieldValues,
-	matchSiteData,
+	matchPostData,
 	adjustFieldOptions,
 	adjustFieldValues,
 ] ) )
 
-const fields = {
-	'Post Title': 'title',
-	'Post URL': 'link',
-	'Post ID': 'id',
-	'Post Slug': 'slug',
-	'Post Excerpt': 'excerpt',
-	'Post Date': 'date',
-	'Post Date GMT': 'date_gmt',
-	'Post Modified': 'modified',
-	'Post Modified GMT': 'modified_gmt',
-	'Post Type': 'type',
-	'Post Status': 'status',
-	'Author Name': 'name',
-	'Author ID': 'author',
-	'Author Posts URL': 'url',
-	'Comment Number': 'comments_num',
-	'Comment Status': 'comment_status',
-	'Featured Image URL': 'featured_image_urls',
-}
-
-//utilize addtoTest
-
-const adjustField = ( fieldName, fieldOptions = {} ) => {
+const adjustPostField = ( fieldName, fieldOptions = {} ) => {
 	cy.adjustDynamicContent( 'ugb/cta', 0, '.ugb-cta__title', {
 		source: 'Latest Post',
 		fieldName,
@@ -42,39 +21,166 @@ const adjustField = ( fieldName, fieldOptions = {} ) => {
 	} )
 }
 
+/**
+ * Setup function.
+ *
+ * This is where we populate `@fieldsToAssert` for
+ * assertions.
+ *
+ * Prefetch field values and store it as alias.
+ *
+ * @example `@fieldsToAssert` structure
+ * ```
+ * [
+ * 		{
+ * 			name: <fieldName>
+ * 			value: <will assert value>,
+ * 			options: { ...<`adjustDynamicContent` command options> }
+ * 		}
+ * ]
+ * ```
+ */
 function setFieldValues() {
-	it( 'should populate WP with 10 test posts', () => {
+	it( 'should populate fields for Latest Post', () => {
 		cy.setupWP()
+		cy.newPost()
+		// Define the alias.
+		cy.wrap( [] ).as( 'fieldsToAssert' )
 
-		range( 10, 0 ).forEach( idx => {
-			cy.newPost()
-			cy.typePostTitle( `Latest Post # ${ idx }` )
+		// Populate Post Title.
+		cy.typePostTitle( 'Latest Post test' )
+		addToTest( { name: 'Post Title', value: 'Latest Post test' } )
 
-			cy.addPostExcerpt( `This is excerpt # ${ idx }` )
+		// Populate Post Slug.
+		const slug = `post-slug-${ ( new Date().getTime() * uniqueId() ) % 1000 }`
+		cy.addPostSlug( slug )
+		addToTest( { name: 'Post Slug', value: slug } )
 
-			const slug = `post-slug-${ ( new Date().getTime() * uniqueId() ) % 1000 }`
-			cy.addPostSlug( slug )
+		// Populate Post Excerpt.
+		cy.addPostExcerpt( 'Excerpt content.' )
+		addToTest( { name: 'Post Excerpt', value: 'Excerpt content.' } )
 
-			cy.addFeaturedImage()
-			//Post Fields
-			//Post Dates
-			//Author Fields
-			//Comment Fields
+		/**
+		 * Populate the following fields:
+		 * - Post URL
+		 * - Post ID
+		 * - Featured Image URL
+		 * - Post Date
+		 * - Post Date GMT
+		 * - Post Modified
+		 * - Post Modified GMT
+		 * - Post Type
+		 * - Post Status
+		 * - Author Name
+		 * - Author ID
+		 * - Author Posts URL
+		 * - Comment Number
+		 * - Comment Status
+		 */
 
-			cy.savePost()
-			//may also assert
+		// Add a featured image.
+		cy.addFeaturedImage()
+		cy.getPostData().then( data => {
+			// Post Fields
+			addToTest( { name: 'Post URL', value: data.link } )
+			addToTest( { name: 'Post ID', value: data.id } )
+			addToTest( { name: 'Featured Image URL', value: data.featured_image_urls.full[ 0 ] } )
+			addToTest( { name: 'Post Type', value: data.type } )
+			addToTest( { name: 'Post Status', value: data.status } )
+
+			/**
+			 * Add a custom format for date values.
+			 */
+			const dateOptions = {
+				'Date Format': 'custom',
+				'Custom Format': 'Y-m-d',
+			}
+
+			addToTest( {
+				name: 'Post Date', value: first( data.date.split( 'T' ) ), options: dateOptions,
+			} )
+			addToTest( {
+				name: 'Post Date GMT', value: first( data.date_gmt.split( 'T' ) ), options: dateOptions,
+			} )
+			addToTest( {
+				name: 'Post Modified', value: first( data.modified.split( 'T' ) ), options: dateOptions,
+			} )
+			addToTest( {
+				name: 'Post Modified GMT', value: first( data.modified_gmt.split( 'T' ) ), options: dateOptions,
+			} )
+
+			// Author Fields
+			addToTest( { name: 'Author Name', value: data.author_info.name } )
+			addToTest( { name: 'Author ID', value: data.author } )
+			addToTest( { name: 'Author Posts URL', value: data.author_info.url } )
+
+			// Misc. Fields
+			addToTest( { name: 'Comment Number', value: parseInt( data.comments_num ).toString() } )
+			addToTest( { name: 'Comment Status', value: data.comment_status } )
+		} )
+
+		// Get the author's profile picture URL
+		cy.wp().then( wp => {
+			addToTest( { name: 'Author Profile Picture URL', value: wp.data.select( 'core' ).getAuthors()[ 0 ].avatar_urls[ 96 ] } )
+		} )
+
+		/**
+		 * Populate the following fields:
+		 *
+		 * - Author First Name
+		 * - Author Last Name
+		 * - Author Posts
+		 */
+		cy.getPostUrls().then( ( { editorUrl } ) => {
+			// Set Author first & last name in profile settings
+			cy.visit( '/wp-admin/profile.php' )
+
+			cy.get( 'tr.user-first-name-wrap input#first_name' ).click( { force: true } ).type( '{selectall}{backspace}Juan' )
+			addToTest( { name: 'Author First Name', value: 'Juan' } )
+
+			cy.get( 'tr.user-last-name-wrap input#last_name' ).click( { force: true } ).type( '{selectall}{backspace}Dela Cruz' )
+			addToTest( { name: 'Author Last Name', value: 'Dela Cruz' } )
+
+			cy.get( 'input[value="Update Profile"]' ).click( { force: true } )
+
+			cy.visit( '/wp-admin/users.php' )
+			cy.get( '[data-colname="Posts"] span[aria-hidden="true"]' ).invoke( 'text' ).then( numberOfPosts => {
+				addToTest( { name: 'Author Posts', value: numberOfPosts } )
+			} )
+			// Go back to the editor.
+			cy.visit( editorUrl )
 		} )
 	} )
 }
-function matchSiteData() {
-	it( 'should match dynamic content in site fields', () => {
-		cy.setupWP()
-		Object.keys( fields ).forEach( fieldName => {
-			cy.newPost()
-			cy.typePostTitle( `${ fieldName } test` )
 
-			cy.addBlock( 'ugb/cta' )
-			adjustField( fieldName ) //add fieldoptions
+/**
+ * Helper function for adding a new entry
+ * in `fieldsToAssert`
+ *
+ * @param {Object} item
+ */
+
+function addToTest( item ) {
+	cy.get( '@fieldsToAssert' ).then( $fta => cy.wrap( [ ...$fta, item ] ).as( 'fieldsToAssert' ) )
+}
+
+function matchPostData() {
+	it( 'should match dynamic content in latest post fields', () => {
+		setFieldValues()
+		cy.get( '@fieldsToAssert' ).then( fieldsToAssert => {
+			fieldsToAssert.forEach( ( {
+				name: fieldName, value, options: fieldOptions = {},
+			} ) => {
+				cy.addBlock( 'ugb/cta' )
+
+				// Adjusting dynamic content
+				adjustPostField( fieldName, fieldOptions )
+
+				// Assert the value.
+				cy.selectBlock( 'ugb/cta' ).assertBlockContent( '.ugb-cta__title', value )
+				cy.deleteBlock( 'ugb/cta' )
+			} )
+			cy.savePost()
 		} )
 	} )
 }
