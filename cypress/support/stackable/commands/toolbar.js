083bc78aac0305fd7a475069a13ea00596e49ab5
legaspielaine <elaineelegaspi@gmail.com>
2021-08-13 16:18:22 +0800

    fix test

--- cypress/support/stackable/commands/toolbar.js
+++ cypress/support/stackable/commands/toolbar.js
@@ -69,20 +69,22 @@ export function adjustDynamicContent( blockName, blockSelector, selector, option
 	}
 
 	cy.adjustToolbar( 'Dynamic Fields', () => {
-		const selectFromSuggestions = option => cy
-			.get( '.stackable-dynamic-content__popover-content' )
-			.contains( containsRegExp( option ) )
-			.parentsUntil( '.components-base-control' )
-			.find( '.stackable-dynamic-content__input-container>input' )
-			.click( { force: true } )
+		const selectFromSuggestions = ( option, value = '' ) => {
+			cy
+				.get( '.stackable-dynamic-content__popover-content' )
+				.contains( containsRegExp( option ) )
+				.parentsUntil( '.components-base-control' )
+				.find( '.stackable-dynamic-content__input-container>input' )
+				.click( { force: true } )
+				.type( `{selectall}${ value }` )
+
+			cy.waitLoader( '.components-spinner' )
+		}
 
 		const selectOption = option => cy
 			.get( '.react-autosuggest__suggestions-container--open' )
 			.contains( containsRegExp( option ) )
 			.click( { force: true } )
-
-		// Wait for popover to appear.
-		cy.waitUntil( done => cy.document().then( doc => done( doc.querySelector( '.stackable-dynamic-content__popover-content' ) ) ) )
 
 		if ( source.length ) {
 			selectFromSuggestions( 'Dynamic Source' )
@@ -91,14 +93,14 @@ export function adjustDynamicContent( blockName, blockSelector, selector, option
 
 		if ( Array( 'Other Posts', 'Latest Post' ).includes( source ) && post.length ) {
 			// Select a post if source is Other Posts / Latest Post
-			selectFromSuggestions( `${ source === 'Other Posts' ? 'Posts/Pages' : 'Nth Latest Post' }` )
+			selectFromSuggestions( `${ source === 'Other Posts' ? 'Posts/Pages' : 'Nth Latest Post' }`, post )
 			cy
 				.get( '.react-autosuggest__suggestions-container--open' )
 				.contains( post )
 				.click( { force: true } )
 		}
 
-		selectFromSuggestions( 'Field' )
+		selectFromSuggestions( 'Field', fieldName )
 		selectOption( fieldName )
 
 		if ( ! isEmpty( fieldOptions ) ) {
