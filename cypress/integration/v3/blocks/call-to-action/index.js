6fb7f91299537465dfe19d826bce449baa8f0775
legaspielaine <elaineelegaspi@gmail.com>
2021-08-25 14:32:22 +0800

    added initial tests for call to action block [skip ci]

---
+++ cypress/integration/v3/blocks/call-to-action/index.js
@@ -0,0 +1,52 @@
+
+/**
+ * External dependencies
+ */
+import {
+	assertBlockExist, blockErrorTest,
+} from '~stackable-e2e/helpers'
+import { stkBlocks } from '~stackable-e2e/config'
+
+export {
+	blockExist,
+	blockError,
+	innerBlocks,
+	typeContent,
+}
+
+function blockExist() {
+	it( 'should show the block', assertBlockExist( 'stackable/call-to-action', '.stk-block-call-to-action' ) )
+}
+
+function blockError() {
+	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'stackable/call-to-action' ) )
+}
+function innerBlocks() {
+	it( 'should allow adding inner blocks', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/call-to-action' )
+		cy.selectBlock( 'stackable/call-to-action' )
+
+		stkBlocks
+			.filter( blockName => blockName !== 'stackable/call-to-action' )
+			.forEach( blockName => cy.addInnerBlock( 'stackable/call-to-action', blockName ) )
+
+		cy.savePost()
+	} )
+}
+
+function typeContent() {
+	it( 'should allow typing in the block', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/call-to-action' ).asBlock( 'ctaBlock', { isStatic: true } )
+
+		cy.typeBlock( 'stackable/call-to-action', '.stk-block-heading__text', 'Test CTA block', 0 )
+			.assertBlockContent( '.stk-block-heading__text', 'Test CTA block' )
+		cy.typeBlock( 'stackable/call-to-action', '.stk-block-text__text', 'Lorem ipsum dolor sit amet.', 0 )
+			.assertBlockContent( '.stk-block-call-to-action .stk-block-text__text', 'Lorem ipsum dolor sit amet.' )
+		cy.typeBlock( 'stackable/call-to-action', '.stk-block-button__inner-text', 'Click here', 0 )
+			.assertBlockContent( '.stk-block-call-to-action .stk-block-button__inner-text', 'Click here' )
+	} )
+}
---
+++ cypress/support/stackable/config/stk-blocks.js
@@ -0,0 +1,20 @@
+export default [
+	'stackable/call-to-action',
+	'stackable/card',
+	'stackable/columns',
+	'stackable/container', // TODO: Will be removed
+	'stackable/count-up',
+	'stackable/feature',
+	'stackable/feature-grid',
+	'stackable/header', // TODO: Renamed to hero
+	'stackable/heading',
+	'stackable/icon',
+	'stackable/icon-label',
+	'stackable/icon-list',
+	'stackable/image',
+	'stackable/notification',
+	'stackable/pricing-box',
+	'stackable/text',
+	'stackable/video-popup',
+	// TODO: More blocks to be added
+]
