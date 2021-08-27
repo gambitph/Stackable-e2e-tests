3cde319c3fa6651b90f1a358fb0962d3b02e5168
legaspielaine <elaineelegaspi@gmail.com>
2021-08-27 12:22:47 +0800

    added other tests for column [skip ci]

---
+++ cypress/integration/v3/blocks/column/index.js
@@ -0,0 +1,63 @@
+
+/**
+ * External dependencies
+ */
+import {
+	assertBlockExist, blockErrorTest,
+} from '~stackable-e2e/helpers'
+import { stkBlocks } from '~stackable-e2e/config'
+
+export {
+	blockExist,
+	blockError,
+	innerBlocks,
+	assertLinking,
+	assertWidth,
+}
+
+function blockExist() {
+	it( 'should show the block', assertBlockExist( 'stackable/columns', '.stk-block-column' ) )
+}
+
+function blockError() {
+	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'stackable/columns' ) )
+}
+
+function innerBlocks() {
+	it( 'should allow adding inner blocks', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/columns' )
+
+		stkBlocks
+			.filter( blockName => blockName !== 'stackable/column' )
+			.forEach( blockName => cy.addInnerBlock( 'stackable/column', blockName, 0 ) )
+
+		cy.savePost()
+	} )
+}
+
+function assertLinking() {
+	it( 'should toggle the linking of the column block', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/columns' )
+		cy.selectBlock( 'stackable/column', 0 ).toggleBlockLinking( true )
+		cy.selectBlock( 'stackable/column', 1 ).toggleBlockLinking( false )
+		cy.selectBlock( 'stackable/column', 1 ).find( '.stk-linking-wrapper > .stk--is-unlinked' ).should( 'exist' )
+	} )
+}
+
+function assertWidth() {
+	it( 'should test the adjustment of width using the tooltip', () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/columns' )
+		cy.selectBlock( 'stackable/column', 1 ).resizeWidth( 25 )
+		cy.selectBlock( 'stackable/column', 1 ).assertComputedStyle( {
+			'': { // Assert the `.is-selected` element
+				'flex-basis': '25%',
+			},
+		}, { assertFrontend: false } )
+	} )
+}
