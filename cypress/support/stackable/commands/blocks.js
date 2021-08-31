9c74cd50f3644069296143adf435e34e8db5338a
blankart <blankartwp@gmail.com>
2021-08-31 16:52:20 +0800

    Revert "Feat: Added delay to type block [skip-ci]"
    This reverts commit 9cd93330ddbec8c2952f3654fb255e00e4300ba8.

--- cypress/support/stackable/commands/blocks.js
+++
@@ -1,9 +0,0 @@
-/**
- * Overwrite Cypress Commands
- */
-Cypress.Commands.overwrite( 'typeBlock', ( originalFn, ...args ) => {
-	const options = args.length === 5 ? args.pop() : {}
-	// Set a delay of 300 after typing the text.
-	options.delay = 300
-	return originalFn( ...args, options )
-} )
