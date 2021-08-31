cfc26e0e8f5c888b9918af5a4bed5c421c46b181
Nico Mendoza <blankartwp@gmail.com>
2021-08-31 12:24:19 +0800

    fix parallelize test

--- scripts/parallelize-specs.js
+++ scripts/parallelize-specs.js
@@ -11,50 +11,52 @@ const cypress = require( 'cypress' )
  * @param {Object} args
  */
 function parallelizeSpecs( args ) {
-	glob( args.spec, {}, function( err, files ) {
-		const splitFiles = files.reduce( ( acc, curr, idx ) => {
-			acc[ idx % args[ 'total-machines' ] ] = ! Array.isArray( acc[ idx % args[ 'total-machines' ] ] ) 
-				? castArray( curr ) 
-				: Array( ...acc[ idx % args[ 'total-machines' ] ], curr )
+	const files = args.spec.split( ',' ).reduce( ( acc, curr ) => {
+		return [ ...acc, ...glob.sync( curr ) ]
+	}, [] )
 
-			return acc
-		}, Array( args[ 'total-machines' ] ) )
+	const splitFiles = files.reduce( ( acc, curr, idx ) => {
+		acc[ idx % args[ 'total-machines' ] ] = ! Array.isArray( acc[ idx % args[ 'total-machines' ] ] ) 
+			? castArray( curr ) 
+			: Array( ...acc[ idx % args[ 'total-machines' ] ], curr )
 
-		const env = _config.env || {}
-		const config = _config.config || {}
-		if ( args.env ) {
-			Object.assign( env, args.env.split( ',' ).reduce( ( acc, curr ) => {
-				const [ key, ...value ] = curr.split( '=' )
-				return Object.assign( acc, { [ key ]: trim( value.join( '=' ), '"' ) } )
-			}, {} ) )
+		return acc
+	}, Array( args[ 'total-machines' ] ) )
+
+	const env = _config.env || {}
+	const config = _config.config || {}
+	if ( args.env ) {
+		Object.assign( env, args.env.split( ',' ).reduce( ( acc, curr ) => {
+			const [ key, ...value ] = curr.split( '=' )
+			return Object.assign( acc, { [ key ]: trim( value.join( '=' ), '"' ) } )
+		}, {} ) )
+	}
+
+	if ( args.config ) {
+		Object.assign( config, args.config.split( ',' ).reduce( ( acc, curr ) => {
+			const [ key, ...value ] = curr.split( '=' )
+			return Object.assign( acc, { [ key ]: trim( value.join( '=' ), '"' ) } )
+		}, {} ) )
+	}
+
+	cypress.run( {
+		browser: args.browser || 'electron',
+		spec: splitFiles[ args[ 'machine-number' ] - 1 ].join( ',' ),
+		config,
+		env,
+	} ).then( result => {
+		if ( result.failures ) {
+			console.error( 'Could not execute tests' )
+			console.error( result.message )
+			process.exit( result.failures )
 		}
 
-		if ( args.config ) {
-			Object.assign( config, args.config.split( ',' ).reduce( ( acc, curr ) => {
-				const [ key, ...value ] = curr.split( '=' )
-				return Object.assign( acc, { [ key ]: trim( value.join( '=' ), '"' ) } )
-			}, {} ) )
-		}
-
-		cypress.run( {
-			browser: args.browser || 'electron',
-			spec: splitFiles[ args[ 'machine-number' ] - 1 ].join( ',' ),
-			config,
-			env,
-		} ).then( result => {
-			if ( result.failures ) {
-				console.error( 'Could not execute tests' )
-				console.error( result.message )
-				process.exit( result.failures )
-			}
-
-			process.exit( result.totalFailed )
-		} )
-			.catch( err => {
-				console.error( err.message )	
-				process.exit( 1 )
-			} )
+		process.exit( result.totalFailed )
 	} )
+		.catch( err => {
+			console.error( err.message )	
+			process.exit( 1 )
+		} )
 }
 
 const args = require( 'args-parser' )( process.argv )
