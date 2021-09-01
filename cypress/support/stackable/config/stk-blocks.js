8495acf1aeb1f21969220db3c163a68ab14bdba3
legaspielaine <elaineelegaspi@gmail.com>
2021-09-01 09:56:43 +0800

    added the new blocks in stk block list [skip ci]

--- cypress/support/stackable/config/stk-blocks.js
+++ cypress/support/stackable/config/stk-blocks.js
@@ -1,19 +1,30 @@
 export default [
+	'stackable/accordion',
+	'stackable/blockquote',
+	'stackable/button-group',
+	'stackable/button',
+	'stackable/icon-button', // button-group parent block
 	'stackable/call-to-action',
 	'stackable/card',
 	'stackable/columns',
-	'stackable/container', // TODO: Will be removed
+	'stackable/container',
 	'stackable/count-up',
+	'stackable/expand',
 	'stackable/feature',
 	'stackable/feature-grid',
-	'stackable/header', // TODO: Renamed to hero
+	'stackable/hero',
 	'stackable/heading',
+	'stackable/icon-box',
 	'stackable/icon',
 	'stackable/icon-label',
 	'stackable/icon-list',
 	'stackable/image',
 	'stackable/notification',
+	'stackable/number-box',
+	'stackable/price',
 	'stackable/pricing-box',
+	'stackable/subtitle',
+	'stackable/testimonial',
 	'stackable/text',
 	'stackable/video-popup',
 	// TODO: More blocks to be added
