eeb111ae534835dbbd8261d2d34272512f9c74f6
legaspielaine <elaineelegaspi@gmail.com>
2021-09-06 09:52:41 +0800

    added variation for columns block [skip ci]

--- cypress/integration/v3/blocks/columns/index.js
+++ cypress/integration/v3/blocks/columns/index.js
@@ -11,11 +11,11 @@ export {
 }
 
 function blockExist() {
-	it( 'should show the block', assertBlockExist( 'stackable/columns', '.stk-block-columns' ) )
+	it( 'should show the block', assertBlockExist( 'stackable/columns', '.stk-block-columns', { variation: 'Two columns; equal split' } ) )
 }
 
 function blockError() {
-	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'stackable/columns' ) )
+	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'stackable/columns', { variation: 'Two columns; equal split' } ) )
 }
 // TODO: Add test for column collapsing in Desktop, Tablet, Mobile
 // New build change: Adding columns in the inspector instead of block appender
