fe8a93055af6bfe0786ceb912e9cda1a11feea73
legaspielaine <elaineelegaspi@gmail.com>
2021-09-28 12:17:34 +0800

    turn on Load Version 2 block in the editor by default

--- cypress/support/stackable/commands/setup.js
+++ cypress/support/stackable/commands/setup.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { containsRegExp } from '~common/util'
+
+/**
  * Register functions to Cypress Commands
  */
 Cypress.Commands.add( 'activateLicense', activateLicense )
@@ -16,6 +21,19 @@ Cypress.Commands.overwrite( 'setupWP', ( originalFn, ...args ) => {
 	if ( Cypress.env( 'STACKABLE_PREMIUM_CODE' ) && ! isStackableActivated ) {
 		cy.activateLicense()
 	}
+
+	// Turn on Load Version 2 blocks in the editor
+	const selectCheckbox = () => cy
+		.get( 'article[id="other-settings"]' )
+		.contains( containsRegExp( 'Load version 2 blocks in the editor' ) )
+		.parent()
+
+	cy.visit( '/wp-admin/options-general.php?page=stackable' )
+	selectCheckbox().then( $parentEl => {
+		if ( ! $parentEl.find( 'svg.components-checkbox-control__checked' ).length ) {
+			selectCheckbox().find( 'input.components-checkbox-control__input' ).click( { force: true } )
+		}
+	} )
 
 	// Upload media to the server
 	cy.uploadMedia()
