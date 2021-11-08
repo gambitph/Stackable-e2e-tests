79214ff7f498460752f42a1771bb1db91fa1b031
legaspielaine <elaineelegaspi@gmail.com>
2021-11-08 15:16:00 +0800

    added accordion style tab tests

--- cypress/integration/v3/blocks/accordion/index.js
+++ cypress/integration/v3/blocks/accordion/index.js
@@ -2,11 +2,13 @@
 /**
  * External dependencies
  */
+import { range } from 'lodash'
 import {
 	assertBlockExist, blockErrorTest, checkJsFiles, assertInnerBlocks, responsiveAssertHelper, Block,
 } from '~stackable-e2e/helpers'
 import { stkBlocks } from '~stackable-e2e/config'
 
+const [ desktopStyle ] = responsiveAssertHelper( styleTab, { disableItAssertion: true } )
 const [ desktopBlock, tabletBlock, mobileBlock ] = responsiveAssertHelper( blockTab, { tab: 'Block', disableItAssertion: true } )
 
 export {
@@ -16,6 +18,7 @@ export {
 	innerBlocksExist,
 	loadedFiles,
 	assertIcon,
+	desktopStyle,
 	desktopBlock,
 	tabletBlock,
 	mobileBlock,
@@ -60,6 +63,64 @@ function assertIcon() {
 		cy.newPage()
 		cy.addBlock( 'stackable/accordion', { variation: 'Default Layout' } )
 		cy.get( '.stk-block-accordion .fa-chevron-down' ).should( 'exist' )
+	} )
+}
+
+function styleTab( viewport, desktopOnly ) {
+	beforeEach( () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/accordion', { variation: 'Default Layout' } ).asBlock( 'accordionBlock', { isStatic: true } )
+		cy.openInspector( 'stackable/accordion', 'Style' )
+		cy.savePost()
+	} )
+
+	it( `should assert General options in ${ viewport }`, () => {
+		desktopOnly( () => {
+			cy.collapse( 'General' )
+			cy.adjust( 'Open at the start', true ).assertClassName( '.stk-block-accordion', 'stk--is-open', { assertBackend: false } )
+			cy.deleteBlock( 'stackable/accordion' )
+
+			range( 1, 4 ).forEach( idx => {
+				cy.addBlock( 'stackable/accordion', { variation: 'Default Layout' } )
+				cy.typeBlock( 'stackable/accordion', '.stk-block-heading__text', `Accordion ${ idx }`, idx - 1 )
+				cy.openInspector( 'stackable/accordion', 'Style', `Accordion ${ idx }` )
+				cy.collapse( 'General' )
+				cy.adjust( 'Close adjacent on open', true )
+			} )
+
+			cy.savePost()
+			cy.getPostUrls().then( ( { previewUrl } ) => {
+				cy.visit( previewUrl )
+				range( 0, 3 ).forEach( idx1 => {
+					cy
+						.get( '.stk-block-accordion' )
+						.eq( idx1 )
+						.find( '.stk-block-heading__text' )
+						.click( { force: true } )
+						.then( () => {
+							range( 0, 3 ).forEach( idx2 => {
+								if ( idx1 !== idx2 ) {
+									cy
+										.get( '.stk-block-accordion' )
+										.eq( idx2 )
+										.invoke( 'attr', 'class' )
+										.then( classNames => {
+											assert.isTrue(
+												! classNames.includes( 'stk--is-open' ),
+												`Expected that Accordion ${ idx2 + 1 } does not have the 'stk--is-open' classname.`
+											)
+										} )
+								}
+							} )
+						} )
+				} )
+			} )
+		} )
+	} )
+
+	afterEach( () => {
+		cy.assertFrontendStyles( '@accordionBlock' )
 	} )
 }
 
