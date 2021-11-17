f37915d62d253c4536ee00c7c04ec6522e66db4f
legaspielaine <elaineelegaspi@gmail.com>
2021-11-18 07:19:33 +0800

    removed assertlink test to be moved to style tab test

--- cypress/integration/v3/blocks/button/index.js
+++ cypress/integration/v3/blocks/button/index.js
@@ -4,7 +4,7 @@
  */
 import { uniqueId } from 'lodash'
 import {
-	assertBlockExist, blockErrorTest, assertLinks, responsiveAssertHelper, Block,
+	assertBlockExist, blockErrorTest, responsiveAssertHelper, Block,
 } from '~stackable-e2e/helpers'
 
 const [ desktopBlock, tabletBlock, mobileBlock ] = responsiveAssertHelper( blockTab, { tab: 'Block', disableItAssertion: true } )
@@ -14,7 +14,6 @@ export {
 	blockError,
 	typeContent,
 	pressEnterKey,
-	assertLink,
 	desktopBlock,
 	tabletBlock,
 	mobileBlock,
@@ -72,36 +71,6 @@ function pressEnterKey() {
 		cy.savePost()
 		// Reloading should not cause a block error
 		cy.reload()
-	} )
-}
-
-function assertLink() {
-	it( 'should assert the links in buttons', () => {
-		cy.setupWP()
-		cy.newPage()
-		cy.typePostTitle( 'Test Button Link' )
-		cy.addBlock( 'stackable/button-group' )
-		cy.selectBlock( 'stackable/button-group' ).addNewColumn( { blockToAdd: 'Button' } )
-
-		// Add a unique e2e className for the 2nd button added
-		// TODO: Find a better way to add this directly when calling `.addNewColumn`
-		cy.selectBlock( 'stackable/button', 1 ).then( $block => {
-			const clientId = $block.data( 'block' )
-			cy.wp().then( wp => {
-				const className = wp.data.select( 'core/block-editor' ).getBlock( clientId ).attributes.className
-				wp.data.dispatch( 'core/block-editor' ).updateBlockAttributes( clientId, { className: `${ className ? className + ' ' : '' }e2etest-block-${ uniqueId() }` } )
-			} )
-		} )
-
-		cy.selectBlock( 'stackable/button', 0 ).asBlock( 'buttonBlock1', { isStatic: true } )
-		cy.selectBlock( 'stackable/button', 1 ).asBlock( 'buttonBlock2', { isStatic: true } )
-
-		cy.typeBlock( 'stackable/button', '.stk-button__inner-text', 'Buy', 0 )
-		cy.typeBlock( 'stackable/button', '.stk-button__inner-text', 'Learn More', 1 )
-		assertLinks( 'stackable/button', {
-			editorSelector: '.stk-button__inner-text',
-			frontendSelector: '.stk-button',
-		} )
 	} )
 }
 
