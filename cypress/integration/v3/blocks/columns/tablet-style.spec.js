a0eef7ea31c86b15365ef204b9454f941a09984b
legaspielaine <elaineelegaspi@gmail.com>
2021-11-25 10:27:55 +0800

    added columns style tab test

---
+++ cypress/integration/v3/blocks/columns/tablet-style.spec.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { registerTests } from '~stackable-e2e/helpers'
+
+/**
+ * Internal dependencies
+ */
+import { tabletStyle } from '.'
+
+describe( 'Columns Block ( Tablet Style )', registerTests( [ tabletStyle ] ) )
