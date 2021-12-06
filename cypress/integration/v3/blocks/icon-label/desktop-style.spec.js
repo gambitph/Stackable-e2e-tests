b414d5c692e54baf9b53e1e07567ae1b2ed26ddf
legaspielaine <elaineelegaspi@gmail.com>
2021-12-06 12:40:06 +0800

    Icon label style tab (#265)
    * Icon label style tab
    
    * run tests
    
    * revert yml [skip ci]

---
+++ cypress/integration/v3/blocks/icon-label/desktop-style.spec.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { registerTests } from '~stackable-e2e/helpers'
+
+/**
+ * Internal dependencies
+ */
+import { desktopStyle } from '.'
+
+describe( 'Icon Label Block ( Desktop Style )', registerTests( [ desktopStyle ] ) )
