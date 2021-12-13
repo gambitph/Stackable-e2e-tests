9b69ae338cf6f92bfb3fd7c13f25575446faa57a
legaspielaine <elaineelegaspi@gmail.com>
2021-12-13 17:30:50 +0800

    Image style tab tests

---
+++ cypress/integration/v3/blocks/image/desktop-style.spec.js
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { registerTests } from '~stackable-e2e/helpers'
+
+/**
+ * Internal dependencies
+ */
+import { desktopStyle } from '.'
+
+describe( 'Image Block ( Desktop Style )', registerTests( [ desktopStyle ] ) )
