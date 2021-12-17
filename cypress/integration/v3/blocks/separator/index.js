e0921de4afb4a0454161598665992aa4b4cd4546
legaspielaine <elaineelegaspi@gmail.com>
2021-12-17 11:11:41 +0800

    Separator style tab (#274)
    * initial tests [skip ci]
    
    * Separator style tab
    
    * revert yml [skip ci]
    
    * uncommented test, rerun
    
    * changed yml to run separator style tests
    
    * revert yml [skip ci]
    
    * removed shadow hover state test
    
    * revert yml [skip ci]

--- cypress/integration/v3/blocks/separator/index.js
+++ cypress/integration/v3/blocks/separator/index.js
@@ -1,16 +1,21 @@
 /**
  * External dependencies
  */
+import { kebabCase } from 'lodash'
 import {
 	assertBlockExist, blockErrorTest, responsiveAssertHelper, Block, Advanced,
 } from '~stackable-e2e/helpers'
 
+const [ desktopStyle, tabletStyle, mobileStyle ] = responsiveAssertHelper( styleTab, { disableItAssertion: true } )
 const [ desktopBlock, tabletBlock, mobileBlock ] = responsiveAssertHelper( blockTab, { tab: 'Block', disableItAssertion: true } )
 const [ desktopAdvanced, tabletAdvanced, mobileAdvanced ] = responsiveAssertHelper( advancedTab, { tab: 'Advanced', disableItAssertion: true } )
 
 export {
 	blockExist,
 	blockError,
+	desktopStyle,
+	tabletStyle,
+	mobileStyle,
 	desktopBlock,
 	tabletBlock,
 	mobileBlock,
@@ -25,6 +30,126 @@ function blockExist() {
 
 function blockError() {
 	it( 'should not trigger block error when refreshing the page', blockErrorTest( 'stackable/separator' ) )
+}
+
+function styleTab( viewport, desktopOnly ) {
+	beforeEach( () => {
+		cy.setupWP()
+		cy.newPage()
+		cy.addBlock( 'stackable/separator' ).asBlock( 'separatorBlock', { isStatic: true } )
+		cy.openInspector( 'stackable/separator', 'Style' )
+		cy.savePost()
+	} )
+
+	afterEach( () => cy.assertFrontendStyles( '@separatorBlock' ) )
+
+	it( 'should assert General panel in Style tab', () => {
+		cy.collapse( 'General' )
+		cy.adjust( 'Height', 327, { viewport } ).assertComputedStyle( {
+			'.stk-block-separator__inner': {
+				'height': '327px',
+			},
+		} )
+		desktopOnly( () => {
+			cy.adjust( 'Flip Horizontally', true )
+			cy.adjust( 'Flip Vertically', true ).assertComputedStyle( {
+				'.stk-block-separator': {
+					'transform': 'scaleX(-1) scaleY(-1)',
+				},
+			} )
+		} )
+	} )
+
+	it( 'should assert Separator panel in Style tab', () => {
+		cy.collapse( 'Separator' )
+		desktopOnly( () => {
+			cy.adjust( 'Design', 'wave-3' ).assertClassName( '.stk-block-separator .stk-separator__layer-1 > path', 'wave-3_svg__st2' )
+			cy.adjust( 'Color', '#59a583' ).assertComputedStyle( {
+				'.stk-block-separator svg': {
+					'fill': '#59a583',
+				},
+			} )
+			cy.adjust( 'Width', 1.8 ).assertComputedStyle( {
+				'.stk-block-separator__inner': {
+					'transform': 'scaleX(1.8)',
+				},
+			} )
+
+			cy.adjust( 'Shadow / Outline', 5 ).assertComputedStyle( {
+				'.stk-block-separator svg': {
+					'filter': 'drop-shadow(0px 2px 20px rgba(153, 153, 153, 0.2))',
+				},
+			} )
+			cy.resetStyle( 'Shadow / Outline' )
+
+			// Adjust Adv. Shadow Options - normal
+			const parentSelector = '.components-popover__content .stk-control-content'
+			// Press the cog symbol to open Shadow settings
+			cy.get( 'button[aria-label="Shadow Settings"]' ).click( { force: true } )
+			cy.adjust( 'Horizontal Offset', 8, { parentSelector } )
+			cy.adjust( 'Vertical Offset', 11, { parentSelector } )
+			cy.adjust( 'Blur', 25, { parentSelector } )
+			cy.adjust( 'Shadow Color', '#2a8a62', { parentSelector } )
+			cy.adjust( 'Shadow Opacity', 0.6, { parentSelector } ).assertComputedStyle( {
+				'.stk-block-separator svg': {
+					'filter': 'drop-shadow(8px 11px 25px rgba(42, 138, 98, 0.6))',
+				},
+			} )
+
+			cy.adjust( 'Invert Design', true )
+			cy.adjust( 'Flip Horizontally', true )
+			cy.adjust( 'Flip Vertically', true )
+			cy.adjust( 'Bring to Front', true ).assertComputedStyle( {
+				'.stk-block-separator': {
+					'transform': 'scaleX(-1) scaleY(-1)',
+					'z-index': '6',
+				},
+			} )
+		} )
+
+		cy.adjust( 'Height', 281, { viewport } ).assertComputedStyle( {
+			'.stk-block-separator__inner': {
+				'height': '281px',
+			},
+		} )
+	} )
+
+	const layers = [ 'Layer 2', 'Layer 3' ]
+
+	layers.forEach( layer => {
+		it( `should assert ${ layer } panel in Style tab`, () => {
+			cy.toggleStyle( layer )
+			desktopOnly( () => {
+				cy.adjust( 'Color', '#ecf56a' ).assertComputedStyle( {
+					[ `.stk-block-separator__inner .stk-separator__${ kebabCase( layer ) }` ]: {
+						'fill': '#ecf56a',
+					},
+				} )
+				cy.adjust( 'Layer Width', 2.4 )
+				cy.adjust( 'Flip Horizontally', true ).assertComputedStyle( {
+					[ `.stk-block-separator__inner .stk-separator__${ kebabCase( layer ) }` ]: {
+						'transform': 'scaleX(-1) scaleX(2.4)',
+					},
+				} )
+				cy.resetStyle( 'Layer Width' )
+				cy.adjust( 'Flip Horizontally', false )
+
+				cy.adjust( 'Opacity', 0.5 )
+				cy.adjust( 'Mix Blend Mode', 'multiply' ).assertComputedStyle( {
+					[ `.stk-block-separator__inner .stk-separator__${ kebabCase( layer ) }` ]: {
+						'opacity': '0.5',
+						'mix-blend-mode': 'multiply',
+					},
+				} )
+			} )
+
+			cy.adjust( 'Layer Height', 1.19, { viewport } ).assertComputedStyle( {
+				[ `.stk-block-separator__inner .stk-separator__${ kebabCase( layer ) }` ]: {
+					'transform': 'scaleY(1.19)',
+				},
+			} )
+		} )
+	} )
 }
 
 const assertBlockTab = Block
